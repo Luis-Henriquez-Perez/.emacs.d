6ba6584e131af54f0ae2a8cd5ae4673e33488930
Luis Henriquez <luis@luishp.xyz>
2020-12-04 14:26:05 -0500

    Update init

--- init.el
+++ init.el
@@ -4388,7 +4388,7 @@ Orderless will do this."
 ;; :ID:       6d02f80a-6d77-4a02-911e-98b7f4004048
 ;; :END:
 
-(void-autoload 'keychord #'keychord-mode)
+(autoload #'keychord-mode "keychord" nil t nil)
 
 (alet (list #'evil-insert-state #'evil-emacs-state)
   (void-load-before-call 'keychord it))
