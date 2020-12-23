4ac32fc30d9eeaded5d9d636e16797923eafbd31
Luis Henriquez <luis@luishp.xyz>
2020-12-23 11:48:35 -0500

    Use org-element to tangle.

--- init.el
+++ init.el
@@ -1,57 +1,31 @@
 ;; -*- lexical-binding: t -*-
 
-;; The primary goal of this file is to tangle [[file:main.org][main.org]] if it
-;; hasn't been tangled already.
+(defun void--needs-tangling-p (org-file elisp-file)
+  "Return non-nil if ORG-FILE needs to be tangled into ELISP-FILE"
+  (or (not (file-exists-p elisp-file))
+      (file-newer-than-file-p org-file elisp-file)))
 
-;; * Startup Optimizations
+(defun void--org-parse-fn (src-block)
+  "Return the string for the body of the source block."
+  (org-element-property :value src-block))
 
-;; ** Built-in Libraries
+(defun void-source-block-strings (org-file)
+  (with-temp-buffer
+    (insert-file-contents org-file)
+    (org-element-map (org-element-parse-buffer) '(src-block) #'void--org-parse-fn)))
 
-;; ** helpers
-
-;; *** check if tangling is needed
-
-;; I only want to tangle =VOID-MAIN-ORG-FILE= when actually need to. Tangling is
-;; done at the expense of some (small) initialization time increase. If it can be
-;; avoided, all the better.
-
-(defun void-needs-tangling-p ()
-  "Whether `VOID-MAIN-ORG-FILE' needs to be tangled.
-Tangling needs to occur when either `VOID-MAIN-ELISP-FILE' does not exist or
-`VOID-MAIN-ORG-FILE' is newer than `VOID-MAIN-ELISP-FILE'."
-  (or (not (file-exists-p VOID-MAIN-ELISP-FILE))
-      (file-newer-than-file-p VOID-MAIN-ORG-FILE VOID-MAIN-ELISP-FILE)))
-
-;; *** tangling
-
-;; ** tangle from and to files
-
-(defconst VOID-MAIN-ORG-FILE (concat user-emacs-directory "config.org")
-  "Org file containing most of VOID's initialization code.")
-
-(defconst VOID-MAIN-ELISP-FILE (concat user-emacs-directory "main.el")
-  "The elisp file that `VOID-MAIN-ORG-FILE' tangles to.")
-
-;; ** main body
-
-(defun void-tangle-org-file ()
-  (let ((code ""))
-    (org-babel-map-src-blocks VOID-MAIN-ORG-FILE
-      (setq code (concat code body "\n")))
-    (with-temp-file VOID-MAIN-ELISP-FILE
-      (insert ";; -*- lexical-binding: t -*-\n\n")
-      (insert code))))
-
-(defun void-tangle-org-file-maybe ()
-  (when (void-needs-tangling-p)
-    (require 'org)
-    (void/tangle-org-file)))
-
-(defun void/tangle-org-file ()
-  (interactive)
-  (void-tangle-org-file))
+(defun void--tangle-org-file (org-file elisp-file)
+  (with-temp-file elisp-file
+    (insert ";; -*- lexical-binding: t -*-\n\n")
+    (insert (string-join (void-source-block-strings org-file) "\n"))))
 
 (let ((file-name-handler-alist nil)
-      (gc-cons-threshold most-positive-fixnum))
-  (void-tangle-org-file-maybe)
-  (load VOID-MAIN-ELISP-FILE nil t))
+      (gc-cons-threshold most-positive-fixnum)
+      (org-file (concat user-emacs-directory "config.org"))
+      (elisp-file (concat user-emacs-directory "main.el")))
+  (when (void--needs-tangling-p org-file elisp-file)
+    (require 'subr-x)
+    (require 'org)
+    (require 'org-element)
+    (void--tangle-org-file org-file elisp-file))
+  (load elisp-file nil t))
