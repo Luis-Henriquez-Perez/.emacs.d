596cabb38be0cf5d2d3fe35c014235f9d8fff216
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-07 17:54:16 -0500

    init.el

--- init.el
+++ init.el
@@ -61,6 +61,7 @@
 ;;;; set load-path
 (add-to-list 'load-path (expand-file-name "lisp/" user-emacs-directory))
 ;;;; load requirements
+(vc-state (buffer-file-name))
 (require 'init-no-littering)
 ;; Built-in packages
 (require 'init-abbrev)
