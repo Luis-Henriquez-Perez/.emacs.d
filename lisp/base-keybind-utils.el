989d1c31e447a553a8daef97af58cff48fbc197d
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-12 21:20:34 -0500

    lisp/base-keybind-utils.el

--- lisp/base-keybind-utils.el
+++ lisp/base-keybind-utils.el
@@ -61,7 +61,7 @@
 (defun oo-bind (keymap key def &optional states description)
   "Bind KEY to DEF in KEYMAP."
   (cond ((symbolp keymap)
-         (info! "Symbol %s")
+         (info! "Deferring binding because Symbol %s")
          (oo-call-after-bound keymap #'apply #'oo-bind arguments))
         (t
          (oo--do-binding #'evil-define-key* states keymap key def)
