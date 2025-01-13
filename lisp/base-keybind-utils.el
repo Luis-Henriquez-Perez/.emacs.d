90c851e85001a557adcc21a484f2c139904f6011
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-12 19:24:56 -0500

    lisp/base-keybind-utils.el

--- lisp/base-keybind-utils.el
+++ lisp/base-keybind-utils.el
@@ -31,6 +31,7 @@
 ;; 5. Log the binding of keys.
 ;; The macro variant should declare the functions that are being bound so I do
 ;; not have to do that.
+;; 6. Gracefully handle errors with binding keys.
 ;;
 ;; I have written a binding function in the past.  And I do not know I think a
 ;; function would be better than a macro.  I do not know at the very least a
