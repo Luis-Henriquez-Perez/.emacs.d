452e8f513c4f657b56b19f59f12ee29176e880e3
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-13 16:20:09 -0500

    init.el

--- init.el
+++ init.el
@@ -55,12 +55,8 @@
   `(condition-case err
        (let ((start-time ))
          (require ,feature)
-
-         (message "Loaded %s in %.2fms"
-                  ',feature
-                  )
-         ()
-         )
+         `    (* 1000 (float-time (time-subtract (current-time) start-time)))
+         (message "Loaded %s in %.2fms" ',feature))
      (error
       (message "Error loading %s: %s" ',feature (error-message-string err)))))
 ;;;; load requirements
