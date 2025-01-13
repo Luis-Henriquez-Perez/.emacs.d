9e3b23b3e9ff5bb9b1e527b5d5e0a722e7f75594
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-13 17:12:11 -0500

    init.el

--- init.el
+++ init.el
@@ -63,7 +63,8 @@
      (setq total-time (* 1000 (float-time (time-subtract (current-time) start-time))))
      (message "Loaded %s in %.2fms" ',feature total-time)))
 
-(defmacro init! ())
+(defmacro init! ()
+  )
 ;;;; load requirements
 (require! init-no-littering)
 ;; Built-in packages
