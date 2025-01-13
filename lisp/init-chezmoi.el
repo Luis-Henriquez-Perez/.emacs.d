435dc9009a565d86ac70959ef154ac0d4daa7f10
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-13 14:10:05 -0500

    Cleanup chezmoi file.

--- lisp/init-chezmoi.el
+++ lisp/init-chezmoi.el
@@ -27,51 +27,9 @@
 ;;; Code:
 (require 'base)
 
-(autoload #'chezmoi-find                       "chezmoi"                           nil t 'function)
-(autoload #'chezmoi-write                      "chezmoi"                           nil t 'function)
-(autoload #'chezmoi-open-other                 "chezmoi"                           nil t 'function)
-
-;; I need the command to write the source from the target.  The command
-;; =chezmoi-apply= does this but I would like it to do it automatically if I am
-;; already editing a target-file.
-;; (progn
-;;   (defun after-save-hook&chezmoi-write-maybe
-;;       (&rest args)
-;;     (info! "Running hook %s..." 'after-save-hook&chezmoi-write-maybe)
-;;     (condition-case err
-;;         (progn "Add the Tempel Capf to `completion-at-point-functions'."
-;;                (setq-local completion-at-point-functions
-;;                            (cons
-;;                             (function tempel-expand)
-;;                             completion-at-point-functions)))
-;;       (error
-;;        (if oo-debug-p
-;;            (signal
-;;             (car err)
-;;             (cdr err))
-;;          (error! "Error %s calling %s in %s because of %s" 'after-save-hook 'after-save-hook&chezmoi-write-maybe
-;;                  (car err)
-;;                  (cdr err))))))
-;;   (add-hook 'after-save-hook
-;;             (function after-save-hook&chezmoi-write-maybe)
-;;             nil nil))
-
-
-;; I use the command =chezmoi-write= the most so far.  It syncs the current file
-;; with its corresponding chezmoi file.  If called while in the target file, it
-;; applies the changes in the target file to the source file and vice versa.
-;; Only caveat is that if there is a more recent change in the "other" file,
-;; then you have to use a prefix command to make sure you want to override those
-;; changes.
-
-;; Binding to the "w" key is the more BLANK choice but "d" is closer to the
-;; homerow for QWERTY keyboards.
-
-;; The command =chezmoi-open-other= is also useful.  Similar to =chezmoi-find=
-;; it does something different depending on whether your in the source file or
-;; the target file.  If you are in the source file, you open the target file and
-;; vice versa.
-
+(autoload #'chezmoi-find "chezmoi" nil t 'function)
+(autoload #'chezmoi-write "chezmoi" nil t 'function)
+(autoload #'chezmoi-open-other "chezmoi" nil t 'function)
 ;;; provide
 (provide 'init-chezmoi)
 ;;; init-chezmoi.el ends here
