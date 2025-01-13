162f2dd08497ce5f23dff613d158c1b4c70bf3be
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-12 19:23:31 -0500

    lisp/config-org-agenda.el

--- lisp/config-org-agenda.el
+++ lisp/config-org-agenda.el
@@ -64,7 +64,13 @@
   "Comparators used for sorting org agenda.
 This is a more flexible replacement for `org-agenda-sorting-strategy'.")
 
-
+(setq +org-agenda-comparators '(+org-agenda-priority-comparator
+                                +org-agenda-closest-deadline-comparator
+                                +org-agenda-tag-comparator
+                                +org-agenda-effort-comparator
+                                +org-agenda-tsid-comparator)
+      "Comparators used for sorting org agenda.
+This is a more flexible replacement for `org-agenda-sorting-strategy'.")
 ;;;;; helpers
 (defun! +org-agenda-call-at-entry (entry fn)
   "Call function from entry."
