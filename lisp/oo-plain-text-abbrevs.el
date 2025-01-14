21d60d93bb6da1160cb6612a753f53859d6f2f43
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-14 08:37:22 -0500

    lisp/oo-plain-text-abbrevs.el

--- lisp/oo-plain-text-abbrevs.el
+++ lisp/oo-plain-text-abbrevs.el
@@ -51,7 +51,7 @@ string or comment."
          ;; this never happens for a string.
          (>= word-beg comment-beg)))))
 ;;;; define abbrevs
-(define-abbrev global-abbrev-table "occuu"         "refactor"                      nil :enable-function #'oo--use-plain-text-abbrev-p)
+(define-abbrev global-abbrev-table "occuu"         "occurrences"                      nil :enable-function #'oo--use-plain-text-abbrev-p)
 (define-abbrev global-abbrev-table "refactor"         "refactor"                      nil :enable-function #'oo--use-plain-text-abbrev-p)
 (define-abbrev global-abbrev-table "ver"              "version"                       nil :enable-function #'oo--use-plain-text-abbrev-p)
 (define-abbrev global-abbrev-table "projet"           "project"                       nil :enable-function #'oo--use-plain-text-abbrev-p)
