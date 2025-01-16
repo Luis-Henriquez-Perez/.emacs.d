ab4a348ba7c4d0c0880d476b381928564a441dd4
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-16 08:14:24 -0500

    lisp/base-keybind-utils.el

--- lisp/base-keybind-utils.el
+++ lisp/base-keybind-utils.el
@@ -59,7 +59,7 @@
 
 (defmacro bind! (&rest args)
   (if states
-      `(oo-bind :states ',states ',keymap ,key ,def)
+      `(oo-bind :states ',states :keymap ',keymap ,key ,def)
     '(oo-bind ',states ',keymap ,key ,def)))
 
 (defun! oo-localleader-bind (keymap key def)
