05f5865c23ed2322407e066aaa712e920d742ca2
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-15 23:19:14 -0500

    lisp/base-keybind-utils.el

--- lisp/base-keybind-utils.el
+++ lisp/base-keybind-utils.el
@@ -64,7 +64,7 @@
          (info! "is not bound, deferring...")
          (oo-call-after-bound keymap #'apply #'oo-bind arguments))
         (states
-         (oo--do-binding #'evil-define-key* states keymap key def)
+         (dolist (state states) (oo--do-binding #'evil-define-key* states keymap key def))
          )
         (t
          (oo--do-binding #'define-key keymap key def))))
