0606b0e7bb5302e376f1610a199cf6870477d948
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-15 23:15:01 -0500

    lisp/base-keybind-utils.el

--- lisp/base-keybind-utils.el
+++ lisp/base-keybind-utils.el
@@ -56,6 +56,8 @@
 
 ;; What data structure should I use to record bindings?
 
+(bind! i "A-x" #'execute-extended-command)
+
 (defun oo-bind (keymap key def &key states)
   "Bind KEY to DEF in KEYMAP."
   (cond ((symbolp keymap)
