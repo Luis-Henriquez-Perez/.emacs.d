a22a5b557afc2a9ffbd8902c74127081b96d23c3
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-20 22:59:36 -0500

    lisp/oo-keybindings.el

--- lisp/oo-keybindings.el
+++ lisp/oo-keybindings.el
@@ -185,7 +185,7 @@
 (bind! oo-leader-map "o" #'oo-org-prefix-command :wk "org")
 (bind! oo-leader-map "j" #'oo-org-prefix-command :wk "org")
 
-(bind! oo-org-map "p you" #'+org-capture-todo)
+(bind! oo-org-map "p u" #'org-priority-up)
 (bind! oo-org-map "t" #'+org-capture-todo)
 (bind! oo-org-map "j" #'+org-capture-todo)
 (bind! oo-org-map "a" #'org-archive-subtree)
