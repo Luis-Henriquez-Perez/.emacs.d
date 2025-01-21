ded6b0d6d67356360fbe456e6b4990c526926dec
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-21 04:10:34 -0500

    lisp/04-base-lib.el

--- lisp/04-base-lib.el
+++ lisp/04-base-lib.el
@@ -22,7 +22,7 @@
 ;;
 ;;; Commentary:
 ;;
-;; Compared to `base-utils' this library has functions that on external
+;; Compared to `03-base-utils' this library has functions that on external
 ;; packages loaded in `base-requirements'.
 
 ;; This file contains functions and macros directly used for customizing Emacs
