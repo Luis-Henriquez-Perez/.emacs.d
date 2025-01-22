8d673ce3a2625457c8d4f00380107775a1131057
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-22 09:40:03 -0500

    lisp/oo-keybindings.el

--- lisp/oo-keybindings.el
+++ lisp/oo-keybindings.el
@@ -132,7 +132,7 @@
 (bind! (n m v) override-global-map ";" #'execute-extended-command)
 
 (defvar-keymap oo-leader-map
-  :prefix 'oo-leader--map
+  :prefix 'oo-leader-map-prefix-command
   ";" #'+org-agenda-day-view
   oo-normal-leader-key #'execute-extended-command
   "b" #'oo-buffer-prefix-command
