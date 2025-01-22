bea1c457d00eb6d4d08d442cad3e0d7d4d498135
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-21 20:41:51 -0500

    lisp/base-utils.el

--- lisp/base-utils.el
+++ lisp/base-utils.el
@@ -268,7 +268,7 @@ Specifically, return the symbol `string' if point is in a string, the symbol
           (t nil))))
 ;;;; logging
 (defun oo-log (type message &rest args)
-  (message "[%s]"))
+  (message "[%s]" type))
 ;;; provide
 (provide 'base-utils)
 ;;; base-utils.el ends here
