6a3a5429520eb0c496b35fa46c6853542e000583
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-22 14:17:53 -0500

    lisp/base-keybind-utils.el

--- lisp/base-keybind-utils.el
+++ lisp/base-keybind-utils.el
@@ -63,6 +63,7 @@
          (oo--bind #'keymap-set (list keymap key def) which-key))))
 
 (defmacro bind! (&rest args)
+  (pcase args)
   (cond (states
          `(oo-bind ',keymap ,key ,def :states ',states :which-key ,which-key))
         (t
