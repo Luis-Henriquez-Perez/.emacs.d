5ad4885fe6edee08117b88c1222cc7a978bc4ea8
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-22 14:05:00 -0500

    lisp/base-keybind-utils.el

--- lisp/base-keybind-utils.el
+++ lisp/base-keybind-utils.el
@@ -63,7 +63,6 @@
          (oo--bind #'keymap-set (list keymap key def) which-key))))
 
 (defmacro bind! (&rest args)
-  ;; The first arguments are a list of states.
   (cond (states
          `(oo-bind ',keymap ,key ,def :states ',states :which-key ,which-key))
         (t
