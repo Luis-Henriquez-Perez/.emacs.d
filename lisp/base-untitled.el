3dd550288e21aa239f6d77f7557fceb8c853f4dd
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-21 19:47:45 -0500

    lisp/base-untitled.el

--- lisp/base-untitled.el
+++ lisp/base-untitled.el
@@ -30,7 +30,7 @@
   "List of logs.")
 
 (defun oo-log (type message &rest args)
-  (when (and (car oo-logs))
+  (when (and (car oo-logs) ())
     (push)))
 ;;;; hooks
 (defun! oo--hook-docstring (hook function)
