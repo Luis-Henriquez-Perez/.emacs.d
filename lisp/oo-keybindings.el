03a11c6be8d1573a5b986b175516f411c0b0b2a3
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-22 13:36:36 -0500

    lisp/oo-keybindings.el

--- lisp/oo-keybindings.el
+++ lisp/oo-keybindings.el
@@ -200,6 +200,7 @@
 ;;;;; toggle
 (defvar oo-toggle-map (make-sparse-keymap)
   "Keymap that contains bindings for things that should be toggled.")
+
 (define-prefix-command 'oo-toggle-prefix-command 'oo-toggle-map)
 
 (defvar-keymap oo-toggle-map
