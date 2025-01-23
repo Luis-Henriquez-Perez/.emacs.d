3b11921bcc7a4b8803f5eb939c23b28108a95302
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-23 08:11:50 -0500

    lisp/base-keybind-utils.el

--- lisp/base-keybind-utils.el
+++ lisp/base-keybind-utils.el
@@ -81,8 +81,8 @@
 
 (defun! oo-localleader-bind (keymap key def)
   "Convenience function for defining localleader bindings."
-  (flet! leader (leader) (kbd (concat leader "\s" key)))
-  (oo-bind keymap (leader oo-emacs-localleader-key) def)
+  (concat leader "\s" key)
+  (oo-bind keymap leade def)
   (oo-bind keymap (leader oo-emacs-localleader-key) def :states 'emacs)
   (oo-bind normal keymap (leader oo-normal-localleader-key) def)
   (oo-bind normal keymap (leader oo-normal-localleader-short-key) def)
