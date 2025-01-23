839a9294d44b59ba5585567e88c105d92fee66ca
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-23 07:43:31 -0500

    lisp/base-keybind-utils.el

--- lisp/base-keybind-utils.el
+++ lisp/base-keybind-utils.el
@@ -66,7 +66,7 @@
   (flet! letter-p ())
   (flet! states-p (it) ())
   (flet! state-p (it) (member it '(n m v i o e)))
-  (flet! keymap-p (it) (and (symbolp it)))
+  (flet! keymap-p (it) (and (symbolp it) (string-match-p )))
   (flet! key-p (it) (or (stringp it) (vectorp it)))
   (pcase args
     (`(,(and (pred state-p) state) ,keymap ,key ,def ,which-key)
