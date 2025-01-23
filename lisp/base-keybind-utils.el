3af936abf114e7ae7be92580b1001b0b83c4de75
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-23 07:41:30 -0500

    lisp/base-keybind-utils.el

--- lisp/base-keybind-utils.el
+++ lisp/base-keybind-utils.el
@@ -64,7 +64,7 @@
 
 (defmacro bind! (&rest args)
   (flet! letter-p ())
-  (flet! states-p (it)    ())
+  (flet! states-p (it) ())
   (flet! state-p (it) (member it '(n m v i o e)))
   (flet! keymap-p (it) (and (symbolp it)))
   (flet! key-p (it) (or (stringp it) (vectorp it)))
