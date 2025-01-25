ee5c279a2b6fca5db52d082844fd2d61de9d51e5
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-25 17:10:23 -0500

    lisp/oo-init.el

--- lisp/oo-init.el
+++ lisp/oo-init.el
@@ -342,6 +342,23 @@ faces immediately."
 
 (add-hook 'enable-theme-functions #'oo-apply-custom-faces-h)
 ;;;; locking
+;;;;; prevent =*Messages*= and =*scratch*= buffers from being killed
+;; "Locking" a file can mean two different things (or both of these things at
+;; once).  It can mean that Emacs cannot be exited while there are "locked"
+;; buffers; it can also mean that the locked buffers cannot be killed (e.g. via
+;; [[file:snapshots/_helpful_command__kill-buffer_.png][kill-buffer]]).  I don't think I ever want the former behavior.  Setting
+;; [[][emacs-default-locking-mode]] to kill tells Emacs just to prevent buffers
+;; with =emacs-lock-mode= enabled from being killed.  If you were to try to kill
+;; one with something like =kill-buffer=, it would fail and you'd get a message
+;; saying the buffer cannot be killed.
+
+;; The =*Messages*= buffer could contain important information and should never
+;; really be killed. See [[https://www.emacswiki.org/emacs/ProtectingBuffers][ProtectingBuffers]].
+(setq emacs-lock-default-locking-mode 'kill)
+(with-current-buffer "*Messages*" (emacs-lock-mode 1))
+
+;; This buffer is useless.  I do not want it taking up space.
+(kill-buffer "*scratch*")
 ;;; provide
 (provide 'oo-init)
 ;;; oo-init.el ends here
