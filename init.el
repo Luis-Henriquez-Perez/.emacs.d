df485c3a9efcbc914c5da10f059e17cb32c49da8
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-26 10:59:34 -0500

    init.el

--- init.el
+++ init.el
@@ -54,7 +54,7 @@
      ()))
 
 (defmacro init! (dir)
-  (dolist (directory-files ,dir )
+  (dolist (directory-files dir t )
     `(require! ',feature path)))
 ;;;; set load-path
 (add-to-list 'load-path (expand-file-name "lisp/" user-emacs-directory))
