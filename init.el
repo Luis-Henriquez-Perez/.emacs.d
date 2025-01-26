dc622b3d47aa6284897a0ed41760e91ce2c0f497
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-25 19:56:21 -0500

    init.el

--- init.el
+++ init.el
@@ -52,6 +52,7 @@
 (add-to-list 'load-path (expand-file-name "lisp/base/" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "lisp/init/" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "lisp/config/" user-emacs-directory))
+(require 'init-loader)
 ;;;; load requirements
 (require 'oo-keybindings)
 (require 'oo-autoloads)
