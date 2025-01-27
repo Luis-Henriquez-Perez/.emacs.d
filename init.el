d46a28145ac09c56ec268af62645e467d42ed217
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-27 13:00:49 -0500

    init.el

--- init.el
+++ init.el
@@ -50,7 +50,8 @@
 ;;;; loader
 (defvar oo-init-info nil)
 (defun oo-startup-time-table ()
-  (interactive))
+  (interactive)
+  )
 ;; Sort init info by the amount of time taken.  Then put the longest times on
 ;; top.  Compute the total and place the percentages.
 ;; (apply #'+ )
