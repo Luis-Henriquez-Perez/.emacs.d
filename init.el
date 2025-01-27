beb435c5383721b5eab578645ea468998150afdb
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-27 10:16:05 -0500

    init.el

--- init.el
+++ init.el
@@ -49,7 +49,7 @@
 (setq-default mode-line-format nil)
 ;;;; loader
 (defvar oo-init-info nil)
-;; Sort init info by the amount of time taken.  Then
+;; Sort init info by the amount of time taken.  Then put the longest times on top.
 ;; (defun oo-)
 (defmacro require! (feature &optional path)
   "Catch any errors, record and log the time taken to require FEATURE."
