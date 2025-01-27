7891124c0f7eb39bffd892b5de1c15352aad1a47
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-27 14:01:16 -0500

    lisp/base/base-utils.el

--- lisp/base/base-utils.el
+++ lisp/base/base-utils.el
@@ -81,7 +81,7 @@ This behaves like `/`, but the result is always a floating-point number."
   (declare (pure t) (side-effect-free error-free))
   (apply #'/ (float (car args)) (cdr args)))
 
-(defun oo- (item list)
+(defun oo-affix (item list)
   (append list (list item)))
 ;;;; type conversion
 (defun oo-into-string (&rest args)
