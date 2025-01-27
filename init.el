5612e5f4386c872d99d83ed11a69c32ad0d0f87a
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-27 13:05:21 -0500

    init.el

--- init.el
+++ init.el
@@ -67,7 +67,7 @@
            (message "Required '%s in %.2f seconds" ',feature time))
        (error
         (message "Error requiring '%s: %s" ',feature err)))
-     (push )))
+     (push (list :feature feature :time time))))
 
 (defmacro init! (dir)
   (let (forms feature)
