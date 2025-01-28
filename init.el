ce40648e5bceb303199abb105315230e587c81ae
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-27 20:11:27 -0500

    init.el

--- init.el
+++ init.el
@@ -98,7 +98,7 @@
   (set! total (apply #'+ (mapcar #'cl-second oo-init-data)))
   (flet! total-per (time) (round (* 100 (oo-float-divide time total))))
   (flet! init-per (time) (round (* 100 (oo-float-divide time (string-to-number (emacs-init-time "%.2f"))))))
-  (flet! percent (time) (format "%3d%%" 10))
+  (flet! percent (time) (format "%3d%%" time))
   (setq data (sort data (lambda (o1 o2) (> (cl-second o1) (cl-second o2)))))
   (for! ((feature time) oo-init-data)
     (collecting! data (list feature time (percent (total-per time)) (percent (init-per time)))))
