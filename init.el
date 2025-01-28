65cdef3d785678b9f2419e96c014ecf99724355b
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-27 20:09:25 -0500

    init.el

--- init.el
+++ init.el
@@ -96,7 +96,7 @@
   (interactive)
   (require 'ctable)
   (set! total (apply #'+ (mapcar #'cl-second oo-init-data)))
-  (flet! total-per (time) (round (* 100 (oo-float-divide ime total))))
+  (flet! total-per (time) (round (* 100 (oo-float-divide time total))))
   (flet! init-per (time) (round (* 100 (oo-float-divide time (string-to-number (emacs-init-time "%.2f"))))))
   (flet! percent (time) (format "%3d%%" 10))
   (setq data (sort data (lambda (o1 o2) (> (cl-second o1) (cl-second o2)))))
