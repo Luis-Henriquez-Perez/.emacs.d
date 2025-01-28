f990034278cd245efde477d83668b2a965a1466d
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-27 20:18:25 -0500

    init.el

--- init.el
+++ init.el
@@ -54,13 +54,6 @@
   `(let ((start (current-time))
          (feature ',feature)
          (time nil))
-     (condition-case err
-         (progn
-           (require ',feature ,path)
-           (setq time (string-to-number (format "%.2f" (float-time (time-subtract (current-time) start)))))
-           (message "Required '%s in %f seconds" feature time))
-       (error
-        (message "Error requiring '%s: %s" feature err)))
      (push (list feature time) oo-init-data)))
 
 ;; Sort init info by the amount of time taken.  Then put the longest times on
