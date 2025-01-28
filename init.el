1d528f88d0aff5ff6aa7bd37b9e06ae550fd34f1
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-27 20:20:29 -0500

    init.el

--- init.el
+++ init.el
@@ -58,7 +58,7 @@
          (progn
            (require ',feature ,path)
            (setq time (string-to-number (format "%.2f" (float-time (time-subtract (current-time) start)))))
-           (message "Form '%S in %f seconds" feature time))
+           (message "Form '%S in %f seconds" form time))
        (error
         (message "Error requiring '%s: %s" feature err)))
      (push (list feature time) oo-init-data)))
