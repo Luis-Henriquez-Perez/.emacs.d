ce67f1268879b4ffde183069647a7d8212963be6
Luis Henriquez-Perez <luis@luishp.xyz>
2025-01-27 20:34:41 -0500

    init.el

--- init.el
+++ init.el
@@ -59,8 +59,7 @@
            (setq time (string-to-number (format "%.2f" (float-time (time-subtract (current-time) start)))))
            (message "Form '%S in %f seconds" form time))
        (error
-        (message "Error evaluating '%S: %s" form err)))
-     (push (list feature time) oo-init-data)))
+        (message "Error evaluating %S: %s" form err)))))
 
 ;; Sort init info by the amount of time taken.  Then put the longest times on
 ;; top.  Compute the total and place the percentages.
