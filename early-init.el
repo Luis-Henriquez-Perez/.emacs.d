95ce12b9814eb540f1c2b4267fec5ef7ce3bfdbd
Luis Henriquez-Perez <luis@luishp.xyz>
2025-02-02 02:24:09 -0500

    Stop byte-compilation warnings.

--- early-init.el
+++ early-init.el
@@ -58,8 +58,8 @@
 ;; Prevent unwanted runtime compilation for gccemacs (native-comp) users;
 ;; packages are compiled ahead-of-time when they are installed and site files
 ;; are compiled when gccemacs is installed.
-(setq native-comp-deferred-compilation nil ;; obsolete since 29.1
-      native-comp-jit-compilation nil)
+(defvar native-comp-jit-compilation)
+(setq native-comp-jit-compilation nil)
 
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
