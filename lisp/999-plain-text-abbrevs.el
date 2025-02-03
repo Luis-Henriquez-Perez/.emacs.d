1e645ddebc2ed247a6ad9859c6bee3c6203cd7b3
Luis Henriquez-Perez <luis@luishp.xyz>
2025-02-03 10:39:54 -0500

    Add abbrev for garbage collection.

--- lisp/999-plain-text-abbrevs.el
+++ lisp/999-plain-text-abbrevs.el
@@ -51,6 +51,7 @@ string or comment."
          ;; this never happens for a string.
          (>= word-beg comment-beg)))))
 ;;;; define abbrevs
+(define-abbrev global-abbrev-table "gc"               "garbage collection"            nil :enable-function #'oo--use-plain-text-abbrev-p)
 (define-abbrev global-abbrev-table "comiler"          "compiler"                      nil :enable-function #'oo--use-plain-text-abbrev-p)
 (define-abbrev global-abbrev-table "suceded"          "succeeded"                     nil :enable-function #'oo--use-plain-text-abbrev-p)
 (define-abbrev global-abbrev-table "genl"             "generally"                     nil :enable-function #'oo--use-plain-text-abbrev-p)
