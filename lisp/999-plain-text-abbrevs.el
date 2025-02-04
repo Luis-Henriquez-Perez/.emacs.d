c8b676b975c1260e129283aa9a3db7b75968d365
Luis Henriquez-Perez <luis@luishp.xyz>
2025-02-04 12:21:08 -0500

    Add more abbrevs.

--- lisp/999-plain-text-abbrevs.el
+++ lisp/999-plain-text-abbrevs.el
@@ -51,6 +51,8 @@ string or comment."
          ;; this never happens for a string.
          (>= word-beg comment-beg)))))
 ;;;; define abbrevs
+(define-abbrev global-abbrev-table "ness"             "necessary"                     nil :enable-function #'oo--use-plain-text-abbrev-p)
+(define-abbrev global-abbrev-table "hnadle"           "handle"                        nil :enable-function #'oo--use-plain-text-abbrev-p)
 (define-abbrev global-abbrev-table "anonl"            "anonymous lambda"              nil :enable-function #'oo--use-plain-text-abbrev-p)
 (define-abbrev global-abbrev-table "toh"              "to have"                       nil :enable-function #'oo--use-plain-text-abbrev-p)
 (define-abbrev global-abbrev-table "qute"             "quote"                         nil :enable-function #'oo--use-plain-text-abbrev-p)
