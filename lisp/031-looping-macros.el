85e6945705a83cccb0a37032d9e64e0bcff76408
Luis Henriquez-Perez <luis@luishp.xyz>
2025-02-14 14:25:37 -0500

    Add commentary.

--- lisp/031-looping-macros.el
+++ lisp/031-looping-macros.el
@@ -1,4 +1,4 @@
-;;; 031-looping-macros.el --- TODO: add commentary -*- lexical-binding: t; -*-
+;;; 031-looping-macros.el --- Looping macros -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (c) 2024 Free Software Foundation, Inc.
 ;;
@@ -22,7 +22,7 @@
 ;;
 ;;; Commentary:
 ;;
-;; TODO: add commentary
+;; This is.
 ;;
 ;;; Code:
 (require 'pcase)
