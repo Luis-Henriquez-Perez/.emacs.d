c5bb303fe82c2d65d5467eb08e4ad58748c7c335
Luis Henriquez-Perez <luis@luishp.xyz>
2025-02-14 14:25:47 -0500

    Add more abbrevs.

--- lisp/999-plain-text-abbrevs.el
+++ lisp/999-plain-text-abbrevs.el
@@ -51,6 +51,12 @@ string or comment."
          ;; this never happens for a string.
          (>= word-beg comment-beg)))))
 ;;;; define abbrevs
+(define-abbrev global-abbrev-table "evt"              "everything"                    nil :enable-function #'oo--use-plain-text-abbrev-p)
+(define-abbrev global-abbrev-table "everythig"        "everything"                    nil :enable-function #'oo--use-plain-text-abbrev-p)
+(define-abbrev global-abbrev-table "tna"              "to no avail"                   nil :enable-function #'oo--use-plain-text-abbrev-p)
+(define-abbrev global-abbrev-table "ihht"             "I have had to"                 nil :enable-function #'oo--use-plain-text-abbrev-p)
+(define-abbrev global-abbrev-table "ntb"              "need to be"                    nil :enable-function #'oo--use-plain-text-abbrev-p)
+(define-abbrev global-abbrev-table "tntb"             "that need to be"               nil :enable-function #'oo--use-plain-text-abbrev-p)
 (define-abbrev global-abbrev-table "wanring"          "warning"                       nil :enable-function #'oo--use-plain-text-abbrev-p)
 (define-abbrev global-abbrev-table "reproduca"        "reproducibility"               nil :enable-function #'oo--use-plain-text-abbrev-p)
 (define-abbrev global-abbrev-table "xpand"            "expand"                        nil :enable-function #'oo--use-plain-text-abbrev-p)
