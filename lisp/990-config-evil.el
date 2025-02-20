32a22011c4b4a0def3a7406d520e9c2abfd6a481
Luis Henriquez-Perez <luis@luishp.xyz>
2025-02-20 13:21:15 -0500

    Update custom code to enter evil-insert-state.

--- lisp/990-config-evil.el
+++ lisp/990-config-evil.el
@@ -102,19 +102,19 @@
     (unless (equal bg (face-attribute 'cursor :background))
       (set-cursor-color bg))))
 ;;;; insert state hook
-(defun +evil-enter-insert-state-h ()
+(defun oo--enter-evil-insert-state-maybe (&rest _)
   "Enter insert state if `evil-mode' is enabled."
   (when (bound-and-true-p evil-mode)
     (evil-insert-state 1)))
 ;;;; cross-configuration
 ;;;;; org-capture
-(hook! org-capture-mode-hook +evil-enter-insert-state-h)
+(hook! org-capture-mode-hook oo--enter-evil-insert-state-maybe)
 ;;;;; git-commit
 ;; Note that I cannot use `evil-set-initial-state' for this because
 ;; `git-commit-mode' is a minor-mode.
-(hook! git-commit-mode-hook +evil-enter-insert-state-h)
+(hook! git-commit-mode-hook oo--enter-evil-insert-state-maybe)
 ;;;;; denote
-(hook! denote-after-new-note-hook +evil-enter-insert-state-h)
+(hook! denote-after-new-note-hook oo--enter-evil-insert-state-maybe)
 ;;;;; corfu
 ;; When using evil, neither `corfu-map' nor `tempel-map' bindings will work
 ;; because the maps are overridden by evil.  In order for them to work, we need
