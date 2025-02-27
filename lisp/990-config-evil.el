fefade8429030ec784ab38614500eeae00154d91
Luis Henriquez-Perez <luis@luishp.xyz>
2025-02-27 14:13:41 -0500

    Delete function defined multiple times.

--- lisp/990-config-evil.el
+++ lisp/990-config-evil.el
@@ -131,11 +131,6 @@
   (evil-make-overriding-map tempel-map))
 
 (advice-add 'tempel-insert :after #'oo--enter-evil-insert-state-maybe)
-
-(defun oo--enter-evil-insert-state-maybe (&rest _)
-  (when (bound-and-true-p evil-mode)
-    (evil-insert-state 1)))
-;;;;; auto-insert
 ;;;;; magit
 ;; Note that I cannot use `evil-set-initial-state' for this because
 ;; `git-commit-mode' is a minor-mode.
