9d4e9d6959d4e67d7ba920fb2f3c4b8e204bec2a
Luis Henriquez-Perez <luis@luishp.xyz>
2025-03-28 16:34:09 -0400

    Avoid byte-compilation warnings.

--- lisp/130-init-burly.el
+++ lisp/130-init-burly.el
@@ -33,6 +33,7 @@
 
 ;; The mode `burly-tabs-mode' causes opening a burly bookmark to enable
 ;; `tab-bar-mode' automatically and create a new tab.
+(declare-function burly-tabs-mode "burly")
 (oo-call-after-load 'burly #'burly-tabs-mode)
 ;;; provide
 (provide '130-init-burly)
