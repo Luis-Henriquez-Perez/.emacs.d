37de9bf33b7ea768ee3ecb57e9b1f60e3f54340a
Luis Henriquez-Perez <luis@luishp.xyz>
2025-03-28 16:42:52 -0400

    Enable dimmer-mode.

--- lisp/130-init-dimmer.el
+++ lisp/130-init-dimmer.el
@@ -32,7 +32,13 @@
 ;;; Code:
 (require '050-base)
 
+;; TODO exclude if it is a which-key buffer.
+;; TODO also exclude if it is the only buffer except for the minibuffer.
 (opt! dimmer-fraction 0.5)
+
+;; TODO make this happen only after I open another window.  So I will need
+;; something like an open window hook.
+(hook! oo-first-input-hook dimmer-mode)
 ;;; provide
 (provide '130-init-dimmer)
 ;;; 130-init-dimmer.el ends here
