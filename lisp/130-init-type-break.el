9447b19d3edded527baa7aa3ca7b43a6963172ac
Luis Henriquez-Perez <luis@luishp.xyz>
2025-03-28 16:45:07 -0400

    Add new files.

---
+++ lisp/130-init-type-break.el
@@ -0,0 +1,38 @@
+;;; 130-init-type-break.el --- TODO: add commentary -*- lexical-binding: t; -*-
+;;
+;; Copyright (c) 2024 Free Software Foundation, Inc.
+;;
+;; Author: Luis Henriquez-Perez <luis@luishp.xyz>
+;; Homepage: https://github.com/Luis-Henriquez-Perez/dotfiles/
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+;;
+;;; Commentary:
+;;
+;; TODO: add commentary
+;;
+;;; Code:
+(opt! type-break-interval (* 0.26 60))  ;; 25 minutes in seconds
+(opt! type-break-good-rest-interval (* 5 60))  ;; 5 minutes in seconds
+
+(opt! type-break-warning-repeat 3)  ;; Repeat warnings 3 times
+(opt! type-break-time-warning-intervals '(60 30 10)) ;; Warnings at 60s, 30s, 10s before break
+(opt! type-break-query-mode t) ;; Force confirmation to skip breaks
+
+(opt! type-break-good-rest-interval nil) ;; 5 minutes break required
+;;; provide
+(provide '130-init-type-break)
+;;; 130-init-type-break.el ends here
