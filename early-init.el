968af2a209102b9c6057c55744c815a8d7cdeabd
Luis Henriquez-Perez <luis@luishp.xyz>
2025-04-26 12:17:10 -0400

    Use default fringes.

--- early-init.el
+++ early-init.el
@@ -64,8 +64,8 @@
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
-(push '(left-fringe  . 0) default-frame-alist)
-(push '(right-fringe . 0) default-frame-alist)
+;; (push '(left-fringe  . 0) default-frame-alist)
+;; (push '(right-fringe . 0) default-frame-alist)
 
 (provide 'early-init)
 ;;; early-init.el ends here
