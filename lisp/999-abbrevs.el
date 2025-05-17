c4cab8427585c78e08472370109b02e9415087b4
Luis Henriquez-Perez <luis@luishp.xyz>
2025-05-17 10:00:20 -0400

    Update README.

--- lisp/999-abbrevs.el
+++ lisp/999-abbrevs.el
@@ -36,7 +36,9 @@
 
 (declare-function lorem-ipsum-insert-paragraphs "lorem-ipsum")
 
-(define-abbrev    global-abbrev-table "reame"              "README"                           nil                                   :enable-function #'oo-use-text-abbrev-p)
+(define-abbrev    global-abbrev-table "reedame"          "README"                              nil                                   :enable-function #'oo-use-text-abbrev-p)
+(define-abbrev    global-abbrev-table "reeame"           "README"                              nil                                   :enable-function #'oo-use-text-abbrev-p)
+(define-abbrev    global-abbrev-table "reame"            "README"                              nil                                   :enable-function #'oo-use-text-abbrev-p)
 (define-abbrev    global-abbrev-table "nuo"              "number of"                           nil                                   :enable-function #'oo-use-text-abbrev-p)
 (define-abbrev    global-abbrev-table "noo"              "number of"                           nil                                   :enable-function #'oo-use-text-abbrev-p)
 (define-abbrev    global-abbrev-table "iftt"             "I feel that there"                   nil                                   :enable-function #'oo-use-text-abbrev-p)
@@ -1836,7 +1838,7 @@
 (define-abbrev    global-abbrev-table "pkgs"             "packages"                            nil                                   :enable-function #'oo-use-text-abbrev-p)
 (define-abbrev    global-abbrev-table "pkg"              "package"                             nil                                   :enable-function #'oo-use-text-abbrev-p)
 (define-abbrev    global-abbrev-table "ftmp"             "for the most part"                   nil                                   :enable-function #'oo-use-text-abbrev-p)
-(define-abbrev    global-abbrev-table "readme"          "README"                              nil                                   :enable-function #'oo-use-text-abbrev-p)
+(define-abbrev    global-abbrev-table "readme"           "README"                              nil                                   :enable-function #'oo-use-text-abbrev-p)
 (define-abbrev    global-abbrev-table "sdnt"             "should not"                          nil                                   :enable-function #'oo-use-text-abbrev-p)
 (define-abbrev    global-abbrev-table "cme"              "come"                                nil                                   :enable-function #'oo-use-text-abbrev-p)
 (define-abbrev    global-abbrev-table "imed"             "immediately"                         nil                                   :enable-function #'oo-use-text-abbrev-p)
