a6e4a85bc4a7fac170b44d0a61cc37fd3123178e
Luis Henriquez-Perez <luis@luishp.xyz>
2025-05-19 08:10:27 -0400

    Do not enable flyspell in prog modes because its too slow.

--- lisp/127-hooks.el
+++ lisp/127-hooks.el
@@ -58,7 +58,10 @@
 (hook! prog-mode-hook rainbow-mode)
 (hook! prog-mode-hook hs-minor-mode)
 (unless noninteractive
-  (hook! prog-mode-hook flyspell-prog-mode))
+  ;; Do not use this hook until I fix the flyspell dict message that is
+  ;; displayed.  Also flyspell is slow to start.
+  ;; (hook! prog-mode-hook flyspell-prog-mode)
+  )
 (hook! text-mode-hook visual-line-mode)
 (unless noninteractive
   (hook! text-mode-hook flyspell-mode))
