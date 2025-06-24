fc58bee8bedff59a4b75d59f50eb783b14812724
Luis Henriquez-Perez <luis@luishp.xyz>
2025-06-24 11:13:13 -0400

    For now add settings for unix files.

--- lisp/128-init.el
+++ lisp/128-init.el
@@ -113,6 +113,18 @@ Replace `kill-buffer--possibly-save' as advice."
 ;; Select from xref candidates in minibuffer
 (opt! xref-show-definitions-function #'xref-show-definitions-completing-read)
 (opt! xref-show-xrefs-function #'xref-show-definitions-completing-read)
+;;;; settings for unix
+(add-to-list 'auto-mode-alist '("\\.service\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.timer\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.target\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.mount\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.automount\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.slice\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.socket\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.path\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.netdev\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.network\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.link\\'" . conf-unix-mode))
 ;;;; Give info buffers better names
 ;; This is taken from the package plk.
 (defvar Info-current-file)
