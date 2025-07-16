f1da386500754a4a6b2d66d9b3d2419e795f2844
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-16 15:03:01 -0400

    Add more abbrevs.

--- lisp/999-abbrevs.el
+++ lisp/999-abbrevs.el
@@ -36,6 +36,8 @@
 
 (declare-function lorem-ipsum-insert-paragraphs "lorem-ipsum")
 
+(define-abbrev    global-abbrev-table "vanila"            "vanilla"                                             nil                                   :enable-function #'oo-use-text-abbrev-p)
+(define-abbrev    global-abbrev-table "vannilla"            "vanilla"                                             nil                                   :enable-function #'oo-use-text-abbrev-p)
 (define-abbrev    global-abbrev-table "cimpiled"            "compiled"                                             nil                                   :enable-function #'oo-use-text-abbrev-p)
 (define-abbrev    global-abbrev-table "cont"            "cannot"                                             nil                                   :enable-function #'oo-use-text-abbrev-p)
 (define-abbrev    global-abbrev-table "kbinding"            "keybinding"                                             nil                                   :enable-function #'oo-use-text-abbrev-p)
