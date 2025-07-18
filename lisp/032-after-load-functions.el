22cd6ec0f2051a3694fd717787673938cca1fc42
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-18 13:11:29 -0400

    Update.

--- lisp/032-after-load-functions.el
+++ lisp/032-after-load-functions.el
@@ -44,7 +44,8 @@ after FEATURE is loaded.")
 
 (defun! oo-eval-after-bound-forms (&rest _)
   "Evaluate forms of any bound symbols in `oo-after-bound-forms'."
-  (for! ((&as elt (symbol . forms)) oo-after-bound-forms)
+  (for! (elt oo-after-bound-forms)
+    (set! (symbol . forms) elt)
     (if (boundp symbol)
         (eval `(progn ,@(nreverse forms)) 'lexical)
       (pushing! updated elt)))
