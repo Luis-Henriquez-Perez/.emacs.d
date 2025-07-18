9d30925959f9ac7f629425ee3625f81308f9a5b0
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-18 19:06:12 -0400

    Add another abbrev.

--- lisp/999-abbrevs.el
+++ lisp/999-abbrevs.el
@@ -36,6 +36,7 @@
 
 (declare-function lorem-ipsum-insert-paragraphs "lorem-ipsum")
 
+(define-abbrev    global-abbrev-table "tras"           "trash"                                          nil                                   :enable-function #'oo-use-text-abbrev-p)
 (define-abbrev    global-abbrev-table "scrapt"           "scraped"                                          nil                                   :enable-function #'oo-use-text-abbrev-p)
 (define-abbrev    global-abbrev-table "sko"              "some kind of"                                     nil                                   :enable-function #'oo-use-text-abbrev-p)
 (define-abbrev    global-abbrev-table "lm"               "let me"                                           nil                                   :enable-function #'oo-use-text-abbrev-p)
