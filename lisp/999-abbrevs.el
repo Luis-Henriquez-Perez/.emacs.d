6ca021b500c5d0689a32b9970fd22a9c72bc31b6
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-19 08:21:52 -0400

    Align abbrevs.

--- lisp/999-abbrevs.el
+++ lisp/999-abbrevs.el
@@ -36,9 +36,10 @@
 
 (declare-function lorem-ipsum-insert-paragraphs "lorem-ipsum")
 
-(define-abbrev    global-abbrev-table "iimet"           "it is much easier to"                                          nil                                   :enable-function #'oo-use-text-abbrev-p)
-(define-abbrev    global-abbrev-table "hled"           "highlighted"                                          nil                                   :enable-function #'oo-use-text-abbrev-p)
-(define-abbrev    global-abbrev-table "tras"           "trash"                                          nil                                   :enable-function #'oo-use-text-abbrev-p)
+(define-abbrev    global-abbrev-table "kep"              "keep"                                             nil                                   :enable-function #'oo-use-text-abbrev-p)
+(define-abbrev    global-abbrev-table "iimet"            "it is much easier to"                             nil                                   :enable-function #'oo-use-text-abbrev-p)
+(define-abbrev    global-abbrev-table "hled"             "highlighted"                                      nil                                   :enable-function #'oo-use-text-abbrev-p)
+(define-abbrev    global-abbrev-table "tras"             "trash"                                            nil                                   :enable-function #'oo-use-text-abbrev-p)
 (define-abbrev    global-abbrev-table "scrapt"           "scraped"                                          nil                                   :enable-function #'oo-use-text-abbrev-p)
 (define-abbrev    global-abbrev-table "sko"              "some kind of"                                     nil                                   :enable-function #'oo-use-text-abbrev-p)
 (define-abbrev    global-abbrev-table "lm"               "let me"                                           nil                                   :enable-function #'oo-use-text-abbrev-p)
