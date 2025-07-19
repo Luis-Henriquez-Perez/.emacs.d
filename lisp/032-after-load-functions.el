8b901aba6d24954f791d01bf14987ef8e2390aad
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-19 09:05:49 -0400

    Traverse the bound forms in reverse.

--- lisp/032-after-load-functions.el
+++ lisp/032-after-load-functions.el
@@ -44,7 +44,7 @@ after FEATURE is loaded.")
 
 (defun! oo-eval-after-bound-forms (&rest _)
   "Evaluate forms of any bound symbols in `oo-after-bound-forms'."
-  (for! ((&as elt (symbol . forms)) oo-after-bound-forms)
+  (for! (:reverse (&as elt (symbol . forms)) oo-after-bound-forms)
     (if (boundp symbol)
         (eval `(progn ,@(nreverse forms)) 'lexical)
       (pushing! updated elt)))
