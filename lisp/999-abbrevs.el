636e34d62c0aa8de93b85941ee06fc0933fe2174
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-21 09:28:18 -0400

    Update abbrevs.

--- lisp/999-abbrevs.el
+++ lisp/999-abbrevs.el
@@ -36,10 +36,10 @@
 
 (declare-function lorem-ipsum-insert-paragraphs "lorem-ipsum")
 
-(define-abbrev    global-abbrev-table "comiled"            "compiled"                                nil                                   :enable-function #'oo-use-text-abbrev-p)
-(define-abbrev    global-abbrev-table "imngtha"            "I am going to have a"                                nil                                   :enable-function #'oo-use-text-abbrev-p)
-(define-abbrev    global-abbrev-table "progrmatically"            "programatically"                                nil                                   :enable-function #'oo-use-text-abbrev-p)
-(define-abbrev    global-abbrev-table "frtm"            "furthermore"                                nil                                   :enable-function #'oo-use-text-abbrev-p)
+(define-abbrev    global-abbrev-table "comiled"          "compiled"                                         nil                                   :enable-function #'oo-use-text-abbrev-p)
+(define-abbrev    global-abbrev-table "imngtha"          "I am going to have a"                             nil                                   :enable-function #'oo-use-text-abbrev-p)
+(define-abbrev    global-abbrev-table "progrmatically"   "programatically"                                  nil                                   :enable-function #'oo-use-text-abbrev-p)
+(define-abbrev    global-abbrev-table "frtm"             "furthermore"                                      nil                                   :enable-function #'oo-use-text-abbrev-p)
 (define-abbrev    global-abbrev-table "tdwiw"            "to do what I want"                                nil                                   :enable-function #'oo-use-text-abbrev-p)
 (define-abbrev    global-abbrev-table "muisc"            "music"                                            nil                                   :enable-function #'oo-use-text-abbrev-p)
 (define-abbrev    global-abbrev-table "musc"             "music"                                            nil                                   :enable-function #'oo-use-text-abbrev-p)
