4d38674b9bbb9c77d9f928b18c520c4d53f134e9
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-21 14:07:59 -0400

    lisp/130-init-auto-insert.el

--- lisp/130-init-auto-insert.el
+++ lisp/130-init-auto-insert.el
@@ -39,7 +39,8 @@
 (autoload! oo-auto-insert-org-file-header    "990-config-auto-insert")
 (autoload! oo-auto-insert-script-file-header    "990-config-auto-insert")
 
-(define-auto-insert "\\.el$"   #'oo-auto-insert-script-file-header)
+
+(define-auto-insert "[^./]+$" #'oo-auto-insert-script-file-header)
 (define-auto-insert "\\.el$"   #'oo-auto-insert-elisp-template)
 (define-auto-insert "\\.html$" #'oo-auto-insert-html-template)
 (define-auto-insert "\\.py$"   #'oo-auto-insert-python-file-header)
