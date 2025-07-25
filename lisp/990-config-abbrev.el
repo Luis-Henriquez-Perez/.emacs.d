9b7f88e268d5cf7eb82ceaf22f758019547b8c48
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-25 10:33:07 -0400

    lisp/990-config-abbrev.el

--- lisp/990-config-abbrev.el
+++ lisp/990-config-abbrev.el
@@ -285,7 +285,7 @@ directory.  If it does not exist create it and add it."
       ;;                     "")))
       ;;   (insert (format fmt name expansion hook plist-str) "\n"))
       )
-    ;; This is the newline.
+    ;; This is the last newline.
     (delete-char -1)
     (oo-insert-at-column (current-column) "))\n")
     (emacs-lisp-mode)
