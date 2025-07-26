9f5e9905e5dc389272fd86331109276f13e721c3
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-26 15:21:11 -0400

    lisp/990-config-abbrev.el

--- lisp/990-config-abbrev.el
+++ lisp/990-config-abbrev.el
@@ -245,26 +245,22 @@ directory.  If it does not exist create it and add it."
   (set! relative-path (file-relative-name html-file (file-name-directory (buffer-file-name))))
   (insert (format "[[%s][%s]]" relative-path name-ext)))
 ;;;; updating the abbrevs
+(add-hook 'kill-emacs-hook 'oo-update-abbrev-tables)
 ;; I only want this function to add complex abbrevs.
 (defun oo-add-abbrev (arg)
   "Add the abbrev I mean."
   (interactive "P")
   (add-abbrev text-mode-abbrev-table "text-mode" arg)
-  (run-with-timer 2 nil #'oo-update-abbrev-tables)
-  ;; (add-hook 'kill-emacs-hook 'oo-update-abbrev-tables)
-  )
+  (run-with-timer 2 nil #'oo-update-abbrev-tables))
 
 (defun oo-inverse-add-abbrev (arg)
   "Add the abbrev I mean."
   (interactive "P")
   (inverse-add-abbrev text-mode-abbrev-table "text-mode" (or arg 1))
-  (run-with-timer 2 nil #'oo-update-abbrev-tables)
-  ;; (add-hook 'kill-emacs-hook 'oo-update-abbrev-tables)
-  )
+  (run-with-timer 2 nil #'oo-update-abbrev-tables))
 
 ;; Also do this during idle time if possible to avoid losing abbrevs if Emacs
 ;; crashes.
-
 (defun! oo-update-abbrev-tables ()
   "Update the abbrev tables."
   (dolist (table abbrev-table-name-list)
