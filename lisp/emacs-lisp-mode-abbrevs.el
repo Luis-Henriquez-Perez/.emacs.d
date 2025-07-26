15dbf1c9412fd7bcd3b97747b9b2ac75073ede56
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-26 11:09:14 -0400

    lisp/emacs-lisp-mode-abbrevs.el

--- lisp/emacs-lisp-mode-abbrevs.el
+++ lisp/emacs-lisp-mode-abbrevs.el
@@ -29,6 +29,7 @@
 
 (autoload! oo-expand-elisp-defun "990-snippets")
 (autoload! oo-expand-elisp-defhook "990-snippets")
+(autoload! oo-expand-elisp-defvar "990-snippets")
 
 (define-abbrev-table 'emacs-lisp-mode-abbrev-table
   '(("msgv" "" 'oo-expand-elisp-message-var :enable-function oo-in-elisp-code-p)
