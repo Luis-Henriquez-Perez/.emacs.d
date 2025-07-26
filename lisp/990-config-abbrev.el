341adbe65514467d0257f3e3ec140aa69989494d
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-26 15:15:54 -0400

    lisp/990-config-abbrev.el

--- lisp/990-config-abbrev.el
+++ lisp/990-config-abbrev.el
@@ -245,17 +245,22 @@ directory.  If it does not exist create it and add it."
   (set! relative-path (file-relative-name html-file (file-name-directory (buffer-file-name))))
   (insert (format "[[%s][%s]]" relative-path name-ext)))
 ;;;; updating the abbrevs
+
+(defvar oo-idle-forms nil
+  "Forms that are run one by one during idle time.")
 ;; I only want this function to add complex abbrevs.
 (defun oo-add-abbrev (arg)
   "Add the abbrev I mean."
   (interactive "P")
-  (add-abbrev text-mode-abbrev-table "text-mode" arg))
+  (add-abbrev text-mode-abbrev-table "text-mode" arg)
+  (run-with-timer 2 nil #'oo-update-abbrev-tables))
 
 (defun oo-inverse-add-abbrev (arg)
   "Add the abbrev I mean."
   (interactive "P")
   (inverse-add-abbrev text-mode-abbrev-table "text-mode" (or arg 1)))
 
+(define-abbrev text-mode-abbrev-table "one by one" nil)
 ;; (add-hook 'kill-emacs-hook 'oo-update-abbrev-tables-h)
 ;; Also do this during idle time if possible to avoid lossing abbrevs if Emacs
 ;; crashes.
