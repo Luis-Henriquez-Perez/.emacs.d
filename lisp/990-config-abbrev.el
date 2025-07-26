b10dd8a65a9946f6039a892aafeac39a3381721e
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-26 15:19:17 -0400

    lisp/990-config-abbrev.el

--- lisp/990-config-abbrev.el
+++ lisp/990-config-abbrev.el
@@ -245,9 +245,6 @@ directory.  If it does not exist create it and add it."
   (set! relative-path (file-relative-name html-file (file-name-directory (buffer-file-name))))
   (insert (format "[[%s][%s]]" relative-path name-ext)))
 ;;;; updating the abbrevs
-
-(defvar oo-idle-forms nil
-  "Forms that are run one by one during idle time.")
 ;; I only want this function to add complex abbrevs.
 (defun oo-add-abbrev (arg)
   "Add the abbrev I mean."
