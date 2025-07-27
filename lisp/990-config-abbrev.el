2dfa6096ad3633871db37e4a3dcc74e271567d73
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-27 00:19:27 -0400

    lisp/990-config-abbrev.el

--- lisp/990-config-abbrev.el
+++ lisp/990-config-abbrev.el
@@ -281,7 +281,7 @@ directory.  If it does not exist create it and add it."
             (insert (oo-abbrev-table-string table)))
           (set! backend (car (vc-deduce-fileset nil t 'state-model-only-files)))
           (set! commit-msg (format "Add abbrevs to the %s..." (string-remove-prefix "910-" (file-name-base file))))
-          (message "update table: %S %S %S %S %S" file (vc-state file) (vc-root-dir) backend commit-(message ""))
+          (message "update table: %S %S %S %S %S" file (vc-state file) (vc-root-dir) backend commit-msg)
           (when (equal 'edited (vc-state file))
             (vc-checkin (list file) backend commit-msg)))))))
 
