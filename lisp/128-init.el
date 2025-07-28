f8f418b0d72b682930701a77e324b61d1a7222fa
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-27 21:30:18 -0400

    lisp/128-init.el

--- lisp/128-init.el
+++ lisp/128-init.el
@@ -176,6 +176,7 @@ file that is in a git repo, enale git-gutter-mode."
 (defun oo-load-theme-maybe-h ()
   "Load theme."
   (set! theme oo-startup-theme)
+  (message "theme->%s" theme)
   (when (and theme (custom-theme-p theme))
     (load-theme theme :no-confirm)))
 
