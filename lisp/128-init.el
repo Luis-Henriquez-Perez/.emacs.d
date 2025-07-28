137f6b6ef5fc2cc5241c0298abc747bfcad1f4d8
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-27 22:00:27 -0400

    lisp/128-init.el

--- lisp/128-init.el
+++ lisp/128-init.el
@@ -181,7 +181,7 @@ file that is in a git repo, enale git-gutter-mode."
   (when (and theme (member theme (custom-available-themes)))
     (load-theme theme :no-confirm)))
 
-(add-hook 'after-init-hook #'oo-load-theme-maybe-h)
+(add-hook 'after-init-hook #'oo-load-theme-maybe-h 90)
 ;;; provide
 (provide '128-init)
 ;;; 128-init.el ends here
