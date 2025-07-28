4328a651eee0f0de3b46b1fcb101906ddff03270
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-27 20:50:41 -0400

    lisp/130-init-eshell.el

--- lisp/130-init-eshell.el
+++ lisp/130-init-eshell.el
@@ -43,7 +43,10 @@
 (opt! eshell-banner-message "")
 (opt! eshell-highlight-prompt nil)
 (autoload! oo-eshell-prompt "990-config-eshell")
-;; For now outsource to epe, but later I will make my own.
+;; For now outsource to epe, but later I will make my own.  Also epe uses static
+;; faces by which I mean constant faces, not existing ones that change with
+;; themes.  So the prompt is difficult to read with certain themes, particularly
+;; light themes.
 (opt! eshell-prompt-function 'epe-theme-pipeline)
 ;; This is obsolete as of Emacs 30.1.
 (opt! eshell-prompt-regexp "^[^λ]+λ ")
