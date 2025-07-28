2ab516e229abfd5a4b5376a75a9331e96453751c
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-28 16:18:25 -0400

    lisp/130-init-org.el

--- lisp/130-init-org.el
+++ lisp/130-init-org.el
@@ -124,6 +124,7 @@
 (nmap org-mode-map "T" #'org-todo)
 (nmap org-mode-map "t" #'+org-choose-tags)
 (nmap org-mode-map "R" #'org-refile)
+(nmap org-mode-map "n" #'org-add-note)
 ;;; provide
 (provide '130-init-org)
 ;;; 130-init-org.el ends here
