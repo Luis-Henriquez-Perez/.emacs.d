28d2f5b2bf82f664466c1711a006796b9f7c71ba
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-28 10:43:47 -0400

    Add keybinding for yeetube-play.

--- lisp/130-init-yeetube.el
+++ lisp/130-init-yeetube.el
@@ -26,6 +26,7 @@
 ;;
 ;;; Code:
 (opt! yeetube-download-directory (expand-file-name "~/Videos/"))
+(nmap yeetube-mode-map "p" #'yeetube-play)
 (nmap yeetube-mode-map "a" #'oo-yeetube-download-audio)
 (nmap yeetube-mode-map "v" #'oo-yeetube-download-video)
 (nmap yeetube-mode-map "s" #'yeetube-search)
