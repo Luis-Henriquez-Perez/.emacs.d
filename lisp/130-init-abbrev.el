16087ea291414f6945d3d80ae534747a6d61a1e4
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-28 19:31:19 -0400

    lisp/130-init-abbrev.el

--- lisp/130-init-abbrev.el
+++ lisp/130-init-abbrev.el
@@ -33,7 +33,7 @@ This function is designed to be added to `abbrev-mode-hook'.  It loads all my
 abbrevs and removes itself from the hook."
   (require '910-text-mode-abbrev-table)
   (require '910-emacs-lisp-mode-abbrev-table)
-  (require '990-post-abbrevs)
+  (require '990-post-abbrev)
   (remove-hook 'abbrev-mode-hook #'oo-load-abbrevs-h))
 
 (add-hook 'abbrev-mode-hook #'oo-load-abbrevs-h)
