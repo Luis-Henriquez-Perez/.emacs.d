6ce008a2780f75c0fafebf49fe56f7f34c2dbbe8
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-27 23:26:51 -0400

    lisp/128-init.el

--- lisp/128-init.el
+++ lisp/128-init.el
@@ -173,7 +173,7 @@ file that is in a git repo, enale git-gutter-mode."
 ;; Try to put this at the end.
 (add-hook 'emacs-startup-hook (lambda () (add-hook 'find-file-hook #'oo-dwim-file-rules 90)))
 
-(message "Loading initial theme `%S'..." theme)
+(message "Loading initial theme `%S'..." oo-initial-theme)
 
 (defun! oo-load-theme-maybe-h ()
   "Load theme."
