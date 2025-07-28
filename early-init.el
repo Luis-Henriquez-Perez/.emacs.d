10bb0c0efc36ec1630c8c579c99f7023c5b9f0ba
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-27 21:37:47 -0400

    early-init.el

--- early-init.el
+++ early-init.el
@@ -75,7 +75,8 @@
   "Load a theme passed as --theme THEME on the command line."
   ;; 'switch' is something like "--theme=modus-operandi"
   (message "option -> %S" argi)
-  (when (string-match "--theme=\\([^[:space:]]+\\)" argi)
+  (message "match -> %S" (string-match "^--theme=\\([^[:space:]]+\\)" argi))
+  (when (string-match "^--theme=\\([^[:space:]]+\\)" argi)
     (setq oo-startup-theme (intern (match-string 1)))
     (message "Set oo-startup-theme->%S" oo-startup-theme)
     (setq command-line-args-left (cdr command-line-args-left))
