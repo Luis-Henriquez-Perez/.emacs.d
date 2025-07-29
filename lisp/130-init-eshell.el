36bb0b33c2239a21ae7e4db90778322bb94a4a5e
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-29 07:43:30 -0400

    lisp/130-init-eshell.el

--- lisp/130-init-eshell.el
+++ lisp/130-init-eshell.el
@@ -35,47 +35,14 @@
 (hook! eshell-mode-hook eat-eshell-mode)
 (hook! eshell-mode-hook eshell-syntax-highlighting-mode)
 ;; Do not let me kill the eshell buffer, at least not easily.
-(hook! eshell-mode-hook emacs-lock-mode)
+;; (hook! eshell-mode-hook emacs-lock-mode)
 
 (oo-popup-at-bottom "\\*eshell")
 
-(opt! eshell-banner-message "")
-(opt! eshell-highlight-prompt nil)
-(autoload! oo-eshell-prompt "990-config-eshell")
-;; For now outsource to epe, but later I will make my own.  Also epe uses static
-;; faces by which I mean constant faces, not existing ones that change with
-;; themes.  So the prompt is difficult to read with certain themes, particularly
-;; light themes.
-(autoload! oo-eshell-prompt "990-config-eshell")
-(opt! eshell-prompt-function 'oo-eshell-prompt)
-;; This is obsolete as of Emacs 30.1.
-(opt! eshell-prompt-regexp "^[^λ]+λ ")
-(opt! eshell-hist-ignoredups t)
-;; Prefer external commands over lisp functions.
-(opt! eshell-prefer-lisp-functions t)
-;; Represent buffers as #<buffer-name>
-(opt! eshell-buffer-shorthand t)
-;; boost eshell history-size
-;; Increase the history size from 128 to 1000.
-(opt! eshell-history-size 1000)
-;; By "highlight" eshell does not just mean coloring the font with the
-;; `eshell-prompt' face.  It also makes the prompt read-only.  Strangely, the
-;; prompt is not read-only by default.  Furthermore, there is no way to override
-;; the text properties `eshell-emit-prompt' adds to the prompt without advising
-;; the it.
-(opt! eshell-highlight-prompt t)
-(opt! eshell-hist-ignoredups t)
-;; boost eshell history-size
-;; Increase the history size from 128 to 1000.
-(opt! eshell-history-size 1000)
-;; Stop eshell from printing messages.
 (declare-function eshell-unload-all-modules "eshell")
 (advice-add #'eshell-unload-all-modules :around #'oo-call-quietly-a)
 
 (advice-add #'eshell-mode :around #'oo-call-quietly-a)
-
-(nmap eshell-mode-map "J" #'eshell-previous-prompt)
-(nmap eshell-mode-map "K" #'eshell-next-prompt)
 ;;; provide
 (provide '130-init-eshell)
 ;;; 130-init-eshell.el ends here
