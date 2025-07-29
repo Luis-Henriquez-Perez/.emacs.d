87c4a721689fe9890055f41d383b98a706564def
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-29 05:05:39 -0400

    lisp/990-config-evil-goggles.el

--- lisp/990-config-evil-goggles.el
+++ lisp/990-config-evil-goggles.el
@@ -27,35 +27,47 @@
 ;;; Code:
 (require '050-base)
 (require 'evil-goggles)
+;;;; Remove advices
+;; Now that `evil-goggles' is loaded we do not need the advice.
+(advice-remove 'evil-delete                                #'oo-require-evil-goggles-a)
+(advice-remove 'evil-delete-line                           #'oo-require-evil-goggles-a)
+(advice-remove 'evil-org-delete                            #'oo-require-evil-goggles-a)
+(advice-remove 'evil-yank                                  #'oo-require-evil-goggles-a)
+(advice-remove 'evil-yank-line                             #'oo-require-evil-goggles-a)
+(advice-remove 'evil-change                                #'oo-require-evil-goggles-a)
+(advice-remove 'evil-change-line                           #'oo-require-evil-goggles-a)
+(advice-remove 'evil-change-whole-line                     #'oo-require-evil-goggles-a)
+(advice-remove 'evil-indent                                #'oo-require-evil-goggles-a)
+(advice-remove 'evil-join                                  #'oo-require-evil-goggles-a)
+(advice-remove 'evil-join-whitespace                       #'oo-require-evil-goggles-a)
+(advice-remove 'evil-fill-and-move                         #'oo-require-evil-goggles-a)
+(advice-remove 'evil-shift-left                            #'oo-require-evil-goggles-a)
+(advice-remove 'evil-shift-right                           #'oo-require-evil-goggles-a)
+(advice-remove 'evil-org                                   #'oo-require-evil-goggles-a)
+(advice-remove 'evil-org                                   #'oo-require-evil-goggles-a)
+(advice-remove 'evil-surround-region                       #'oo-require-evil-goggles-a)
+(advice-remove 'evil-commentary                            #'oo-require-evil-goggles-a)
+(advice-remove 'evilnc-comment-operator                    #'oo-require-evil-goggles-a)
+(advice-remove 'evil-replace-with-register                 #'oo-require-evil-goggles-a)
+(advice-remove 'evil-set-marker                            #'oo-require-evil-goggles-a)
+(advice-remove 'evil-record-macro                          #'oo-require-evil-goggles-a)
+(advice-remove 'evil-paste-before                          #'oo-require-evil-goggles-a)
+(advice-remove 'evil-paste-after                           #'oo-require-evil-goggles-a)
+(advice-remove 'lispyville-yank                            #'oo-require-evil-goggles-a)
+(advice-remove 'lispyville-delete                          #'oo-require-evil-goggles-a)
+(advice-remove 'lispyville-change                          #'oo-require-evil-goggles-a)
+(advice-remove 'lispyville-yank-line                       #'oo-require-evil-goggles-a)
+(advice-remove 'lispyville-delete-line                     #'oo-require-evil-goggles-a)
+(advice-remove 'lispyville-change-line                     #'oo-require-evil-goggles-a)
+(advice-remove 'lispyville-delete-char-or-splice           #'oo-require-evil-goggles-a)
+(advice-remove 'lispyville-delete-char-or-splice-backwards #'oo-require-evil-goggles-a)
+(advice-remove 'lispyville-substitute                      #'oo-require-evil-goggles-a)
+(advice-remove 'lispyville-change-whole-line               #'oo-require-evil-goggles-a)
+(advice-remove 'lispyville-join                            #'oo-require-evil-goggles-a)
+(advice-remove '+evil-eval-operator                        #'oo-require-evil-goggles-a)
+(advice-remove '+evil-eval-replace-operator                #'oo-require-evil-goggles-a)
+(advice-remove '+evil-eval-print-operator                  #'oo-require-evil-goggles-a)
 ;;;; register lispyville commands
-(autolet!
- (set! list '((+evil-eval-operator evil-change)
-              (+evil-eval-replace-operator evil-change)
-              (+evil-eval-print-operator evil-change)
-              (lispyville-delete-line evil-delete-line)
-			  (lispyville-yank-line evil-yank-line)
-			  (lispyville-change-line evil-change-line)
-			  (lispyville-delete-char-or-splice evil-delete-char)
-			  (lispyville-delete-char-or-splice-backwards evil-delete-backward-char)
-			  (lispyville-substitute evil-substitute)
-			  (lispyville-change-whole-line evil-change-whole-line)
-			  (lispyville-join evil-join)
-			  (lispyville-change evil-change)
-			  (lispyville-delete evil-delete)
-			  (lispyville-yank evil-yank)))
- (for! ((new old) list)
-   (set! elt (cons new (cdr (assoc old evil-goggles--commands))))
-   (cl-pushnew elt evil-goggles--commands :key #'car))
- (cl-assert (cl-every (lambda (it) (assoc it list)) (mapcar #'car list))))
-;;;; Use a different face
-;; I want to make `evil-goggles-default-face' the same as that of
-;; `spaceline-evil-operator'.  That is what makes the most sense to me because
-;; most of the evil-goggles-commands will perform their function in
-;; evil-operator state--think of yanking, deleting as well as the eval
-;; operators.
-;; TODO: put in an after block.
-(custom-set-faces
- '(evil-goggles-default-face ((t (:inherit spaceline-evil-operator)))))
 ;;; provide
 (provide '990-config-evil-goggles)
 ;;; 990-config-evil-goggles.el ends here
