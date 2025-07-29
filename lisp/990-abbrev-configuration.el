5d78dd34a8b7aef1a8df237f97d21bfa065b073b
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-28 22:12:37 -0400

    lisp/990-abbrev-configuration.el

--- lisp/990-abbrev-configuration.el
+++ lisp/990-abbrev-configuration.el
@@ -28,7 +28,6 @@
 ;;;; requirements
 (require '050-base)
 (require 'abbrev)
-(message "Loaded post abbrev...")
 ;;;; prevent greedy expansion with `backward-word'
 (abbrev-table-put global-abbrev-table :regexp "\\<\\(\\sw+\\)\\Sw*")
 ;;;; make the parents
