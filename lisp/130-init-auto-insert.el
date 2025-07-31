75fd89cccccbacfb380a9516c604331169d8575d
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-31 10:35:49 -0400

    lisp/130-init-auto-insert.el

--- lisp/130-init-auto-insert.el
+++ lisp/130-init-auto-insert.el
@@ -40,7 +40,6 @@
 (autoload! oo-auto-insert-org-file-header    "990-config-auto-insert")
 (autoload! oo-auto-insert-script-file-header "990-config-auto-insert")
 
-
 (define-auto-insert "\\.\\(?:service\\|timer\\)$" #'oo-auto-insert--pound-comment-header)
 (alet! (concat (regexp-quote (expand-file-name "~/.local/bin/")) "[^./]+$")
   (define-auto-insert it #'oo-auto-insert-script-file-header))
