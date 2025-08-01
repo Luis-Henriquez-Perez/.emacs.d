73fba7a17cc702c898c7d25c6f46763bcca42920
Luis Henriquez-Perez <luis@luishp.xyz>
2025-08-01 08:01:25 -0400

    lisp/128-init.el

--- lisp/128-init.el
+++ lisp/128-init.el
@@ -193,7 +193,7 @@ file that is in a git repo, enale git-gutter-mode."
 (defun oo-load-idle-features ()
   "Load one feature from `oo-idle-features' during idle time."
   (awhen! (pop oo-idle-features)
-    (message "Idle loading: %s" feature)
+    (oo-log 'info "Idle loading: %s" feature)
     (require feature)
     (run-with-idle-timer 1 nil #'oo-load-idle-features)))
 
