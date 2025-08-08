221511164c45ff3676cf31ced2dcfd33f50c52f1
Luis Henriquez-Perez <luis@luishp.xyz>
2025-08-08 07:52:24 -0400

    Move to anaphoric macros.

--- early-init.el
+++ early-init.el
@@ -72,34 +72,6 @@
 (push '(left-fringe  . 0) default-frame-alist)
 (push '(right-fringe . 0) default-frame-alist)
 
-;; Syntactic sugar macros to help me with.
-;; The standard way to write this macro is to have it return the element
-;; removed.  A more controversial but potentialy useful way is to write one that
-;; returns the predicate expression.  That way I could additionally perform some
-;; operation on the element.  And worst case I could just return the original
-;; element via (and SUBPRED it).
-(defmacro aremf! (list pred)
-  "Remove the first element that satisfies PRED and return PRED.
-PRED should be a form that evaluates with `it` bound to each element."
-  (declare (indent 1))
-  (let ((glist (gensym "list"))
-        (gpred (gensym "pred"))
-        (grest (gensym "rest")))
-    `(let* ((,glist ,list)
-            (,grest nil)
-            (,gpred nil)
-            (it nil))
-       (while ,glist
-         (setq it (car ,glist))
-         (setq ,gpred ,pred)
-         (if ,gpred
-             (progn
-               (setq ,list (nconc (nreverse ,grest) (cdr ,glist)))
-               (setq ,glist nil)) ; exit loop
-           (push it ,grest)
-           (setq ,glist (cdr ,glist))))
-       ,gpred)))
-
 ;; I need to process the `command-line-args' for font here so that I can set the
 ;; font before the frame is loaded.
 (defvar oo-initial-theme (aremf! command-line-args
