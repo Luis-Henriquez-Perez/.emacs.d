917d6c5b7ef59c36a435d5ec725ce48c2af8d0e7
Luis Henriquez-Perez <luis@luishp.xyz>
2025-09-25 22:43:04 -0400

    Prefer add-hook version oo-add-hook.

--- lisp/990-config-org.el
+++ lisp/990-config-org.el
@@ -59,7 +59,7 @@ are in alphabetical order."
 
 (advice-add 'org-capture :around #'oo--suppress-window-deletion)
 ;;;; Start notaking in insert mode
-(oo-add-hook 'org-log-buffer-setup-hook #'evil-insert-state)
+(add-hook 'org-log-buffer-setup-hook #'evil-insert-state)
 ;;; provide
 (provide '990-config-org)
 ;;; 990-config-org.el ends here
