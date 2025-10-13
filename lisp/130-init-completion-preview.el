769ccec60b550b94cbca7ab4c5eb6343efb22334
Luis Henriquez-Perez <luis@luishp.xyz>
2025-10-13 19:09:30 -0400

    Remove dabbrev code from completion-preview.

--- lisp/130-init-completion-preview.el
+++ lisp/130-init-completion-preview.el
@@ -31,10 +31,6 @@
 
 (opt! completion-preview-minimum-symbol-length 1)
 (opt! completion-preview-commands (cl-adjoin 'org-self-insert-command completion-preview-commands))
-
-(opt! dabbrev-check-all-buffers nil)
-(opt! dabbrev-case-fold-search t)
-(opt! dabbrev-check-other-buffers nil)
 ;;; provide
 (provide '130-init-completion-preview)
 ;;; 130-init-completion-preview.el ends here
