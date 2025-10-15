55f8dccfd589168c36be97b0b2598869bb594ded
Luis Henriquez-Perez <luis@luishp.xyz>
2025-10-15 11:48:49 -0400

    Fix missing error form.

--- lisp/015-call-after-functions.el
+++ lisp/015-call-after-functions.el
@@ -71,7 +71,8 @@ If SYMBOL is already bound FN is called immediately."
   (condition-case err
       (aprog1! (time-elapsed! (require feature))
         (oo-log 'success "Applied configuration to %s in %0.2f seconds" feature it))
-    (oo-log 'error "Failed apply configuration %s : %S -> %S" feature (car err) (cdr err))))
+    (error
+     (oo-log 'error "Failed apply configuration %s : %S -> %S" feature (car err) (cdr err)))))
 
 (defun oo-require-after-load (feature1 feature2)
   "Load FEATURE2 at FEATURE1 has been loaded."
