349cedc0d8d8ebca180e8feb72b04f21e9366f9f
Luis Henriquez-Perez <luis@luishp.xyz>
2025-10-15 09:53:51 -0400

    Add necessary requirement of 001-base-vars.

--- lisp/110-init-package.el
+++ lisp/110-init-package.el
@@ -27,6 +27,7 @@
 ;;
 ;;; Code:
 (require 'cl-lib)
+(require '001-base-vars)
 ;; If this variable is not set beforehand, `package-gnupghome-dir' will not be
 ;; set to the right place.
 (defvar package-user-dir)
