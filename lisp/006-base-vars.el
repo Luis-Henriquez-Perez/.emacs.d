b635fc4c289d7a32ab1687f62f9f547210391524
Luis Henriquez-Perez <luis@luishp.xyz>
2025-10-26 16:44:32 -0400

    Move these to base-variables.

--- lisp/006-base-vars.el
+++ lisp/006-base-vars.el
@@ -72,6 +72,48 @@ built-in-face.  See `oo--enable-theme-functions--set-state-faces-from-theme-h'."
 (defvar oo-init-data nil
   "Initialization data.
 This includes the time that features took to load.")
+;;;; LEADERS
+;; This file provides leaders keys for evil and non-evil states and it binds
+;; these leader keys.
+
+;; These leaders are specifically for evil mode states (not including insert and
+;;                                                          Emacs).  I choose the space (=SPC=) key for evil leaders because it is one of if
+;; not the easiest key to press because of its central placement on the keyboard
+;; and its sheer size--at least on the [[https://en.wikipedia.org/wiki/QWERTY][qwerty]] keyboard that I use.  The choice
+;; of =SPC m= for the major mode specific keys is simply for the pnemonic =m= which
+;; stands for "major mode".  The short major mode prefix key =,= is for cases when I
+;; want to shorten a key binding.  Although obviously not as easy to remember as
+;; =m=, it provides me with one shorter keypress in certain situations.
+(defconst oo-normal-leader-key "SPC"
+  "The evil leader prefix key.")
+
+(defconst oo-normal-localleader-key "SPC m"
+  "The localleader prefix key for major-mode specific commands.")
+
+(defconst oo-normal-localleader-short-key ","
+  "A shorter alternative `oo-localleader-key'.")
+;; These leaders are for evil insert and emacs states as well as vanilla
+;; Emacs.  Note that evil Emacs state is different from vanilla Emacs.  One of the
+;; goals with these bindings is to set up keybindings in the case that I disable
+;; evil mode or in the case that I want to use my bindings in insert or Emacs
+;; state--or even vanilla Emacs.  The choice behind the bindings is the same as
+;; [[id:][before]], except I just prepended the =Meta= (a.k.a. the =Alt= key) to everything.
+(defconst oo-insert-leader-key "M-SPC"
+  "The leader prefix key used for Insert state.")
+
+(defconst oo-insert-localleader-key "M-SPC m"
+  "The localleader prefix key for major-mode specific commands.")
+
+(defconst oo-insert-localleader-short-key "M-,"
+  "A short non-normal `oo-localleader-key'.")
+
+(defconst oo-emacs-leader-key "C-c l"
+  "The leader prefix key used for Emacs states.")
+
+(defconst oo-emacs-alt-leader-key "C-c SPC")
+
+(defconst oo-emacs-localleader-key "C-c l m"
+  "The localleader prefix key for major-mode specific commands.")
 ;;; provide
 (provide '006-base-vars)
 ;;; 006-base-vars.el ends here
