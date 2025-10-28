a52169858bd64178705f756c2124104edb284daa
Luis Henriquez-Perez <luis@luishp.xyz>
2025-10-27 20:02:07 -0400

    Add autoloads for comments objects.

--- lisp/130-init-evil.el
+++ lisp/130-init-evil.el
@@ -106,7 +106,10 @@
 (nvmap! "g x" #'evil-exchange)
 (nvmap! "g X" #'evil-exchange-cancel)
 ;;;; TEXT-OBJECTS
-;; (iotmap! "c" #'evilnc-inner-comment #'evilnc-outer-comment)
+(autoload 'evilnc-inner-comment "evil-nerd-commenter" nil nil 'function)
+(autoload 'evilnc-outer-comment "evil-nerd-commenter" nil nil 'function)
+
+(iotmap! "c" #'evilnc-inner-comment #'evilnc-outer-comment)
 ;; TODO: In "lispy" modes use lispyville-outer-comment instead.
 (iotmap! "a" #'lispyville-inner-comment #'lispyville-outer-comment)
 (iotmap! "h" #'evil-i-syntax #'evil-a-syntax)
