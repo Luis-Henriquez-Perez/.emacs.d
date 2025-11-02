a42878c94ce19860544036910dee4e7555365dd3
Luis Henriquez-Perez <luis@luishp.xyz>
2025-11-02 08:24:37 -0500

    Make sure to set bray to normal state after escape.
    Maybe it should just pop the state instead?

--- lisp/130-init-meep.el
+++ lisp/130-init-meep.el
@@ -46,7 +46,7 @@ non-readonly file buffer, save the buffer."
   (interactive)
   (when (bound-and-true-p evil-mode)
     (evil-normal-state 1))
-  (modaled-set-state 'normal)
+  (bray-state-set 'normal)
   (cond ((minibuffer-window-active-p (minibuffer-window))
 		 (if (or defining-kbd-macro executing-kbd-macro)
 			 (minibuffer-keyboard-quit)
