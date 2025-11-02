063cfa6422822c84fe0d27ca5e3ddc92e52840c5
Luis Henriquez-Perez <luis@luishp.xyz>
2025-11-02 17:37:39 -0500

    Add abbrevs to the text-mode-abbrev-table...

--- lisp/910-text-mode-abbrev-table.el
+++ lisp/910-text-mode-abbrev-table.el
@@ -3525,6 +3525,7 @@
     ("lieuenant" "lieutenant")
     ("lieved" "lived")
     ("liftime" "lifetime")
+    ("ligher" "lighter")
     ("lightyear" "light year")
     ("lightyears" "light years")
     ("likelyhood" "likelihood")
