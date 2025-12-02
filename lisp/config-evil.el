d50db0f25fd93004f12329130b75d72126d5539f
Luis Henriquez-Perez <luis@luishp.xyz>
2025-12-02 14:47:03 -0500

    Change name make to ensure.

--- lisp/config-evil.el
+++ lisp/config-evil.el
@@ -252,7 +252,7 @@ non-readonly file buffer, save the buffer."
 ;; When using evil, neither `corfu-map' nor `tempel-map' bindings will work
 ;; because the maps are overridden by evil.  In order for them to work, we need
 ;; to boost give the maps greater precedence.
-(defafter! o-make-corfu-kbds-work-with-evil (corfu)
+(defafter! o-ensure-corfu-kbds-work-with-evil (corfu)
   (evil-make-overriding-map corfu-map)
   (advice-add 'corfu--setup :after #'o-evil-normalize-keymaps-ignore-args)
   (advice-add 'corfu--teardown :after #'o-evil-normalize-keymaps-ignore-args))
