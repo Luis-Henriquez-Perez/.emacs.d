7646c3485e807e2aab8d9106d657aac99e228a8e
Luis Henriquez-Perez <luis@luishp.xyz>
2025-12-29 09:02:52 -0500

    Use my own variant of bray.

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -203,7 +203,8 @@
                                      (escr :url "https://github.com/Luis-Henriquez-Perez/escr")
                                      (evil-easymotion :url "https://github.com/Luis-Henriquez-Perez/evil-easymotion" :branch "master")
                                      (outli :url "https://github.com/jdtsmith/outli")
-                                     (zone-matrix :url "https://github.com/ober/zone-matrix" :branch "master")))
+                                     (zone-matrix :url "https://github.com/ober/zone-matrix" :branch "master")
+                                     (bray :url "https://codeberg.org/luishp/emacs-bray" :branch "add-support-for-mode-specific-bindings")))
 
 (defvar o-package-alist-cache (expand-file-name "package-alist" o-cache-dir)
   "Cache for package descriptors.")
