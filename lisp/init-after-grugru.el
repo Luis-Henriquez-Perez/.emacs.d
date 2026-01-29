e5d37173952361da118444f904c97db3bd2d4c48
Luis Henriquez-Perez <luis@luishp.xyz>
2026-01-29 17:16:47 -0500

    Add grugru meta generator.

--- lisp/init-after-grugru.el
+++ lisp/init-after-grugru.el
@@ -27,6 +27,8 @@
 ;;; Code:
 (require 'grugru)
 
+(grugru-define-global 'symbol (grugru-metagenerator-keep-case '("north" "east" "west" "south")))
+
 (grugru-define-global 'symbol (grugru-metagenerator-keep-case '("big" "medium" "small")))
 
 (grugru-define-global 'symbol (grugru-metagenerator-keep-case '("word" "sentence" "paragraph")))
