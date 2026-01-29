58304a542d274869696bd2a20f9f5f0ffa0ec031
Luis Henriquez-Perez <luis@luishp.xyz>
2026-01-29 17:09:15 -0500

    Add grugru.

--- lisp/init-after-bray.el
+++ lisp/init-after-bray.el
@@ -113,9 +113,17 @@
 ;; 1. movement
 ;; 2. cut copy paste
 ;; 3. copy a line and paste it above
-(keymap-set o-bray-state-normal-map "d g" #'grugru-forward)
+;; (keymap-set o-bray-state-normal-map "d g" #'grugru-forward)
 (keymap-set o-bray-state-normal-map "d f" #'grugru-forward)
-(put 'grugru-forward 'repeat-map 'o-bray-state-normal-map)
+
+(defvar o-grugru-repeat-map
+  (let ((map (make-sparse-keymap)))
+    ;; (keymap-set map "g" #'grugru-forward)
+    (keymap-set map "f" #'grugru-forward)
+    map))
+
+(put 'grugru-forward 'repeat-map 'o-grugru-repeat-map)
+
 (keymap-set o-bray-state-normal-map "d j" #'meep-insert-open-below)
 (keymap-set o-bray-state-normal-map "d k" #'meep-insert-open-above)
 ;; Make this keybinding similar to cutting a line.
