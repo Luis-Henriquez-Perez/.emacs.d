5aa143538bdf49802e2e3640516e3827ccdca4a3
Luis Henriquez-Perez <luis@luishp.xyz>
2026-02-14 13:38:29 -0500

    Declare rainbow-mode package.

--- lisp/init-pkg-rainbow-mode.el
+++ lisp/init-pkg-rainbow-mode.el
@@ -25,6 +25,8 @@
 ;; Initialize rainbow-mode.
 ;;
 ;;; Code:
+(o-declare-package 'rainbow-mode)
+
 (autoload 'rainbow-mode "rainbow-mode" nil nil 'function)
 (add-hook 'prog-mode-hook #'rainbow-mode)
 ;;; provide
