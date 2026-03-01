5d1090781331ee4de76b7e6864fbc1c9676f5657
Luis Henriquez-Perez <luis@luishp.xyz>
2026-03-01 02:17:37 -0500

    Add file for new packages.

---
+++ lisp/init-pkg-transpose-mark.el
@@ -0,0 +1,32 @@
+;;; init-pkg-transpose-mark.el --- TODO: add commentary -*- lexical-binding: t; -*-
+;;
+;; Copyright (c) 2024 Free Software Foundation, Inc.
+;;
+;; Author: Luis Henriquez-Perez <luis@luishp.xyz>
+;; Homepage: https://github.com/Luis-Henriquez-Perez/dotfiles/
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or at your option any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+;;
+;;; Commentary:
+;;
+;; I use this pkg for it is ability to "tranpose" or exchange a region.
+;; specifically I use the function `transpose-mark-region'.
+;;
+;;; Code:
+(o-declare-package 'transpose-mark)
+;;; provide
+(provide 'init-pkg-transpose-mark)
+;;; init-pkg-transpose-mark.el ends here
