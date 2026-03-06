d2c620c46e7bff0c251d0a0659cf7cd85c61b3ee
Luis Henriquez-Perez <luis@luishp.xyz>
2026-03-06 12:14:26 -0500

    Add another abbrev.

--- lisp/text-mode-abbrev-table.el
+++ lisp/text-mode-abbrev-table.el
@@ -28,6 +28,7 @@
 ;;; Code:
 (require 'abbrev)
 
+(define-abbrev text-mode-abbrev-table "inset" "insert")
 (define-abbrev text-mode-abbrev-table "ggrugru" "grugru")
 (define-abbrev text-mode-abbrev-table "regon" "region")
 (define-abbrev text-mode-abbrev-table "wok" "work")
