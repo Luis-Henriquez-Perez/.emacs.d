58f9ff2db2940bee51e4e98d44084553a6af5aca
Luis Henriquez-Perez <luis@luishp.xyz>
2026-03-07 12:05:15 -0500

    Update modaled setup.

--- lisp/init-pkg-modaled.el
+++ lisp/init-pkg-modaled.el
@@ -26,52 +26,26 @@
 ;;
 ;;; Code:
 (require 'modaled)
-(require 'meep)
 
-(defvar modaled-normal-state-keymap o-state-normal-keymap)
-(defvar modaled-visual-state-keymap o-state-visual-keymap)
-(defvar modaled-insert-state-keymap o-state-insert-keymap)
+(defvar modaled-normal-state-keymap o-keymap-state-normal)
+(defvar modaled-visual-state-keymap o-keymap-state-visual)
+(defvar modaled-insert-state-keymap o-keymap-state-insert)
+
+;; set init state using a function
+(setq modaled-init-state-fn (lambda () (cond ((minibufferp) "insert")
+                                             (t "normal"))))
 
 (defun o-advice--modaled-set-state (state)
   (modaled-set-state (symbol-name state)))
 
-(setq meep-state-insert 'insert)
-(advice-add 'bray-state-stack-push :override #'o-advice--modaled-set-state)
-(advice-add 'bray-state-set :override #'o-advice--modaled-set-state)
-(advice-add 'bray-state-set :override #'o-advice--modaled-set-state)
+;; (advice-add 'bray-state-stack-push :override #'o-advice--modaled-set-state)
+;; (advice-add 'bray-state-set :override #'o-advice--modaled-set-state)
+;; (advice-add 'bray-state-set :override #'o-advice--modaled-set-state)
 
 (modaled-define-state "normal"
   :lighter "[NOR]"
   :cursor-type 'box)
 
-(defun o-dwim-escape ()
-  "Exit out of whatever is happening after escape.
-Enter normal state.  If in minibuffer, exit the minibuffer.  When in a
-non-readonly file buffer, save the buffer."
-  (interactive)
-  (when (require 'modaled)
-    (modaled-set-state "normal"))
-  (cond ((minibuffer-window-active-p (minibuffer-window))
-		 (if (or defining-kbd-macro executing-kbd-macro)
-			 (minibuffer-keyboard-quit)
-           (abort-recursive-edit)))
-		((or defining-kbd-macro executing-kbd-macro) nil)
-        (t
-         (when (and (not buffer-read-only)
-                    (buffer-file-name)
-                    (buffer-modified-p))
-           (save-buffer))
-		 (keyboard-quit)))
-  (when (require 'modaled)
-    (modaled-set-state "normal")))
-
-;; (keymap-set modaled-normal-state-keymap o-key-leader-normal #'o-leader-map)
-
-;; (defvar-keymap modaled-normal-state-keymap
-;;   "<escape>" #'o-dwim-escape)
-
-;; (add-hook 'modaled-normal-state-mode-hook (apply-partially #'set-cursor-color "orange"))
-;; (add-hook 'modaled-insert-state-mode-hook (apply-partially #'set-cursor-color "green"))
 (modaled-define-state "insert"
   :sparse t
   ;; insert state must be no-suppress to support inserting char
@@ -79,22 +53,34 @@ non-readonly file buffer, save the buffer."
   :cursor-type 'bar
   :lighter "[INS]")
 
-(keymap-set modaled-insert-state-keymap "<escape>" #'o-dwim-escape)
+(modaled-define-state "visual"
+  :sparse t
+  :no-suppress t
+  :cursor-type 'box
+  :lighter "[VIS]")
 
-;; set init state using a function
-(setq modaled-init-state-fn (lambda () "normal"))
+(defun o-hook--modaled-enable-visual-state ()
+  "Activate visual state."
+  (modaled-set-state "visual"))
+
+(defun o-hook--modaled-disable-visual-state ()
+  "Deactivate visual state."
+  (modaled-set-state "normal"))
+
+(add-hook 'activate-mark-hook #'o-hook--modaled-enable-visual-state)
+(add-hook 'deactivate-mark-hook #'o-hook--modaled-disable-visual-state)
 
 (modaled-define-substate "vertico"
   :sparse t
   :no-suppress t)
 
+(add-hook 'minibuffer-mode-hook #'modaled-insert-state-mode)
+(add-hook 'o-escape-hook #'modaled-set-init-state)
+
 (modaled-enable-substate-on-state-change "vertico"
-  :states '("insert")
   :pred #'minibufferp)
 
-(modaled-get-substate-mode "vertico")
-;; => modaled-vertico-substate-mode
-(defvar-keymap modaled-org-substate-keymap
+(defvar-keymap modaled-vertico-mode-substate-keymap
   "C-n" #'vertico-scroll-up
   "C-p" #'vertico-scroll-down
   "TAB" #'vertico-next
@@ -105,45 +91,16 @@ non-readonly file buffer, save the buffer."
   "<backtab>" #'vertico-previous
   "C-o" #'embark-act)
 
-(modaled-define-keys
-  :substates '("vertico")
-  :bind
-  '(("C-n" . vertico-scroll-up)
-    ("TAB" . vertico-next)
-    (";" . vertico-quick-exit)
-    ("C-;" . vertico-quick-exit)))
-
-(modaled-define-substate "org")
-(modaled-define-keys
-  :substates '("org")
-  :bind
-  '(("j" . org-next-visible-heading)
-    ("k" . org-previous-visible-heading)))
-
-;; (defun o-state-changed-p ()
-;;   ""
-;;   (message "State changed...")
-;;   t)
-(modaled-enable-substate-on-state-change
-  "org"
-  :states '("normal")
-  :major '(org-mode)
-  ;; :pred #'o-state-changed-p
-  )
-
 ;; update after major mode changes
-(add-hook 'after-change-major-mode-hook
-          (lambda ()
-            (setq modaled--initialized nil)
-            (if (minibufferp)
-                (modaled-set-state "insert")
-              (modaled-initialize))))
-;; update on creation (no major mode change yet)
+(defun o-hook--setup-modaled ()
+  "Setup modal-editing."
+  (setq modaled--initialized nil)
+  (modaled-initialize))
+
+(add-hook 'after-change-major-mode-hook #'o-hook--setup-modaled)
+
 (add-hook 'buffer-list-update-hook #'modaled-initialize-all-buffers)
-;; enable it for all existing buffers
-(modaled-initialize-all-buffers)
-;; manually switch to it
-(modaled-set-init-state)
+(add-hook 'after-init-hook #'modaled-initialize-all-buffers)
 ;;; provide
 (provide 'init-pkg-modaled)
 ;;; init-pkg-modaled.el ends here
