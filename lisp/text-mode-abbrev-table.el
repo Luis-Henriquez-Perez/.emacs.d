7a73d498c5fcb36ffbded5a729da62253f71d555
Luis Henriquez-Perez <luis@luishp.xyz>
2026-03-07 15:22:40 -0500

    Add more abbrevs.

--- lisp/text-mode-abbrev-table.el
+++ lisp/text-mode-abbrev-table.el
@@ -28,6 +28,7 @@
 ;;; Code:
 (require 'abbrev)
 
+(define-abbrev text-mode-abbrev-table "effcy" "efficiency")
 (define-abbrev text-mode-abbrev-table "accing" "accessing")
 (define-abbrev text-mode-abbrev-table "prosed" "processed")
 (define-abbrev text-mode-abbrev-table "Ensre" "ensure")
