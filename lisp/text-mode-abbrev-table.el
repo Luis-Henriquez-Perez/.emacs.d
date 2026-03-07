aab4f5eb3295c9c8ac91c9fed96acb15c20b88c6
Luis Henriquez-Perez <luis@luishp.xyz>
2026-03-07 15:29:38 -0500

    Add new abbrev.

--- lisp/text-mode-abbrev-table.el
+++ lisp/text-mode-abbrev-table.el
@@ -28,6 +28,7 @@
 ;;; Code:
 (require 'abbrev)
 
+(define-abbrev text-mode-abbrev-table "caped" "capitalized")
 (define-abbrev text-mode-abbrev-table "effcy" "efficiency")
 (define-abbrev text-mode-abbrev-table "accing" "accessing")
 (define-abbrev text-mode-abbrev-table "prosed" "processed")
