55695b50ca767dcfe6cc59b40660cf7040852d1e
Luis Henriquez-Perez <luis@luishp.xyz>
2026-03-07 14:46:03 -0500

    Add two more abbrevs to table.

--- lisp/text-mode-abbrev-table.el
+++ lisp/text-mode-abbrev-table.el
@@ -28,6 +28,8 @@
 ;;; Code:
 (require 'abbrev)
 
+(define-abbrev text-mode-abbrev-table "prosed" "processed")
+(define-abbrev text-mode-abbrev-table "Ensre" "ensure")
 (define-abbrev text-mode-abbrev-table "inster" "insert")
 (define-abbrev text-mode-abbrev-table "fnss" "functions")
 (define-abbrev text-mode-abbrev-table "ddocumentation" "documentation")
