679b4f70edfa35fe8f49f05d50fa008ff7bc699f
Luis Henriquez-Perez <luis@luishp.xyz>
2026-03-10 12:51:16 -0400

    Add another abbrev.

--- lisp/text-mode-abbrev-table.el
+++ lisp/text-mode-abbrev-table.el
@@ -28,6 +28,7 @@
 ;;; Code:
 (require 'abbrev)
 
+(define-abbrev text-mode-abbrev-table "anyw" "anyway")
 (define-abbrev text-mode-abbrev-table "repitition" "repetition")
 (define-abbrev text-mode-abbrev-table "geneic" "generic")
 (define-abbrev text-mode-abbrev-table "dets" "determines")
