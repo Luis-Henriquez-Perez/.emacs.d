cbf508f4b63564773b624022e564b7b2d1fb1a5b
Luis Henriquez-Perez <luis@luishp.xyz>
2026-03-15 01:27:47 -0400

    Add more abbrevs.

--- lisp/text-mode-abbrev-table.el
+++ lisp/text-mode-abbrev-table.el
@@ -28,6 +28,7 @@
 ;;; Code:
 (require 'abbrev)
 
+(define-abbrev text-mode-abbrev-table "abbevs" "abbrevs.")
 (define-abbrev text-mode-abbrev-table "locall" "localleader")
 (define-abbrev text-mode-abbrev-table "kwds" "keywords")
 (define-abbrev text-mode-abbrev-table "consstent" "consistent")
