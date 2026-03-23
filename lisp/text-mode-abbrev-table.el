b9688b82f298fe0aa05a5db4c63b2c22e78c2e79
Luis Henriquez-Perez <luis@luishp.xyz>
2026-03-23 19:24:25 -0400

    Add another abbrev.

--- lisp/text-mode-abbrev-table.el
+++ lisp/text-mode-abbrev-table.el
@@ -28,6 +28,7 @@
 ;;; Code:
 (require 'abbrev)
 
+(define-abbrev text-mode-abbrev-table "aand" "and")
 (define-abbrev text-mode-abbrev-table "secss" "seconds")
 (define-abbrev text-mode-abbrev-table "bbut" "but")
 (define-abbrev text-mode-abbrev-table "expainson" "expansion")
